e50f07b245e437446e26c386ada3ccb374d1ed6b
yrom1 <ryanm.inbox@gmail.com>
2021-12-21 09:50:59 -0500

    75

--- 75.py
+++ 75.py
@@ -78,7 +78,36 @@ class Array:
             profit = max(profit, price - buy)
         return profit
 
-# - Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
+
+    """
+    # - Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
+    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+    Example 1:
+
+    Input: nums = [1,2,3,1]
+    Output: true
+    Example 2:
+
+    Input: nums = [1,2,3,4]
+    Output: false
+    Example 3:
+
+    Input: nums = [1,1,1,3,3,4,3,2,4,2]
+    Output: true
+
+
+    Constraints:
+
+    1 <= nums.length <= 105
+    -109 <= nums[i] <= 109
+    """
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+
 # - Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
 # - Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
 # - Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
