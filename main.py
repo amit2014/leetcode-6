47e7377da883538c1bf311a2bfc93a40dce4a74f
yrom1 <ryanm.inbox@gmail.com>
2021-12-21 13:29:07 -0500

    75 + rename `75.py` to `main.py`

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Array:
     Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
     """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """O(n) time, O(n) space"""
         seen = dict()
         for i, x in enumerate(nums):
             if target - x in seen: return [seen[target - x], i]
@@ -72,6 +73,7 @@ class Array:
     0 <= prices[i] <= 104
     """
     def maxProfit(self, prices: List[int]) -> int:
+        """O(n) time, O(1) space"""
         buy, profit = inf, 0
         for price in prices:
             buy = min(buy, price)
@@ -105,6 +107,7 @@ class Array:
     -109 <= nums[i] <= 109
     """
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """O(n) time, O(n) space"""
         return len(nums) != len(set(nums))
 
     """
@@ -137,6 +140,7 @@ class Array:
     Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
     """
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """O(n) time, O(1) space (excluding output array)"""
         ans = [1] * len(nums)
         prefix = suffix = 1
         for i in range(len(nums)):
@@ -146,8 +150,46 @@ class Array:
             suffix *= nums[~i]
         return ans
 
+    """
+    # - Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-# - Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
+    A subarray is a contiguous part of an array.
+
+
+
+    Example 1:
+
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+    Example 2:
+
+    Input: nums = [1]
+    Output: 1
+    Example 3:
+
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+
+
+    Constraints:
+
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+
+
+    Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+    """
+    def maxSubArray(self, nums: List[int]) -> int:
+        """O(n) time, O(1) space"""
+        cur = ans = nums[0]
+        for num in nums[1:]:
+            cur = max(num, cur + num)
+            ans = max(ans, cur)
+        return ans
+
+
 # - Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
 # - Find Minimum in Rotated Sorted Array - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 # - Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
