d01240283bebf9646b06ef2e5e0c2280a19b1251
yrom1 <ryanm.inbox@gmail.com>
2021-12-21 09:48:15 -0500

    75

--- 75.py
+++ 75.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from math import inf
 
 class Array:
     """
@@ -42,8 +43,41 @@ class Array:
             if target - x in seen: return [seen[target - x], i]
             seen[x] = i
 
+    """
+    # - Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-# - Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+    Example 1:
+
+    Input: prices = [7,1,5,3,6,4]
+    Output: 5
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+    Example 2:
+
+    Input: prices = [7,6,4,3,1]
+    Output: 0
+    Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+    Constraints:
+
+    1 <= prices.length <= 105
+    0 <= prices[i] <= 104
+    """
+    def maxProfit(self, prices: List[int]) -> int:
+        buy, profit = inf, 0
+        for price in prices:
+            buy = min(buy, price)
+            profit = max(profit, price - buy)
+        return profit
+
 # - Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
 # - Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
 # - Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
