ebaa1eee4ccc9f596dd665916484c330bb903a3a
yrom1 <ryanm.inbox@gmail.com>
2021-12-21 19:52:14 -0500

    75

--- main.py
+++ main.py
@@ -190,7 +190,44 @@ class Array:
         return ans
 
 
-# - Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
+    """
+    # - Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
+    Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+
+    The test cases are generated so that the answer will fit in a 32-bit integer.
+
+    A subarray is a contiguous subsequence of the array.
+
+
+
+    Example 1:
+
+    Input: nums = [2,3,-2,4]
+    Output: 6
+    Explanation: [2,3] has the largest product 6.
+    Example 2:
+
+    Input: nums = [-2,0,-1]
+    Output: 0
+    Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+
+    Constraints:
+
+    1 <= nums.length <= 2 * 104
+    -10 <= nums[i] <= 10
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    """
+    def maxProduct(self, nums: List[int]) -> int:
+        """O(n) time, O(1) space"""
+        if len(nums) == 0: return 0
+        ans = mn = mx = nums[0]
+        for num in nums[1:]:
+            mn, mx = min(num, mn*num, mx*num) , max(num, mn*num, mx*num)
+            ans = max(mx, ans)
+        return ans
+
+
 # - Find Minimum in Rotated Sorted Array - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 # - Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
 # - 3Sum - https://leetcode.com/problems/3sum/
