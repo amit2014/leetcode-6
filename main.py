d12957ab9e70fd30f59e3287236748487e5d00fb
yrom1 <ryanm.inbox@gmail.com>
2021-12-23 16:57:19 -0500

    75

--- main.py
+++ main.py
@@ -274,8 +274,117 @@ class Array:
         return nums[lo]
 
 
-# - Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
-# - 3Sum - https://leetcode.com/problems/3sum/
+    """# - Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
+    There is an integer array nums sorted in ascending order (with distinct values).
+
+    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+
+
+    Example 1:
+
+    Input: nums = [4,5,6,7,0,1,2], target = 0
+    Output: 4
+    Example 2:
+
+    Input: nums = [4,5,6,7,0,1,2], target = 3
+    Output: -1
+    Example 3:
+
+    Input: nums = [1], target = 0
+    Output: -1
+
+
+    Constraints:
+
+    1 <= nums.length <= 5000
+    -104 <= nums[i] <= 104
+    All values of nums are unique.
+    nums is an ascending array that is possibly rotated.
+    -104 <= target <= 104"""
+    def search(self, nums: List[int], target: int) -> int:
+        """O(log(n)), O(1)"""
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target: return mid
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]: hi = mid - 1
+                else: lo = mid + 1
+            else:
+                if nums[mid] < target <= nums[hi]: lo = mid + 1
+                else: hi = mid - 1
+        return -1
+
+
+    """# - 3Sum - https://leetcode.com/problems/3sum/
+    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+    Notice that the solution set must not contain duplicate triplets.
+
+
+
+    Example 1:
+
+    Input: nums = [-1,0,1,2,-1,-4]
+    Output: [[-1,-1,2],[-1,0,1]]
+    Example 2:
+
+    Input: nums = []
+    Output: []
+    Example 3:
+
+    Input: nums = [0]
+    Output: []
+
+
+    Constraints:
+
+    0 <= nums.length <= 3000
+    -105 <= nums[i] <= 105"""
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def twoSumII(nums: List[int], i: int, res: List[List[int]]):
+            lo, hi = i + 1, len(nums) - 1
+            while lo < hi:
+                sum = nums[i] + nums[lo] + nums[hi]
+                if sum < 0:
+                    lo += 1
+                elif sum > 0:
+                    hi -= 1
+                else:
+                    res.append([nums[i], nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+                    while lo < hi and nums[lo] == nums[lo - 1]:
+                        lo += 1
+
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                twoSumII(nums, i, res)
+        return res
+
+
 # - Container With Most Water - https://leetcode.com/problems/container-with-most-water/
 
 # Binary
