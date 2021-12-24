05e51e5270f9803527a8db65852387af575fca5f
yrom1 <ryanm.inbox@gmail.com>
2021-12-24 09:22:33 -0500

    75

--- main.py
+++ main.py
@@ -537,7 +537,7 @@ class Binary:
     Follow up: If this function is called many times, how would you optimize it?"""
 
     def hammingWeight(self, n: int) -> int:
-        return bin(n).count('1')
+        return bin(n).count("1")
 
     def _hammingWeight(self, n: int) -> int:
         count = 0
@@ -546,7 +546,49 @@ class Binary:
             count += 1
         return count
 
-# - Counting Bits - https://leetcode.com/problems/counting-bits/
+    """# - Counting Bits -
+    # https://leetcode.com/problems/counting-bits/
+    Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary
+    representation of i.
+
+    Example 1:
+
+    Input: n = 2
+    Output: [0,1,1]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+    Example 2:
+
+    Input: n = 5
+    Output: [0,1,1,2,1,2]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+    3 --> 11
+    4 --> 100
+    5 --> 101
+
+    Constraints:
+
+    0 <= n <= 105
+
+    Follow up:
+
+    It is very easy to come up with a solution with a runtime of O(n log n).
+    Can you do it in linear time O(n) and possibly in a single pass?
+    Can you do it without using any built-in function (i.e., like
+    __builtin_popcount in C++)?"""
+
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for x in range(1, n + 1):
+            ans[x] = ans[x & (x - 1)] + 1
+        return ans
+
+
 # - Missing Number - https://leetcode.com/problems/missing-number/
 # - Reverse Bits - https://leetcode.com/problems/reverse-bits/
 
