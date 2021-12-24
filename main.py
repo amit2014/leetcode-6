504a42cfe62e29b4fa4b9fc8ef21b50fcaf848d0
yrom1 <ryanm.inbox@gmail.com>
2021-12-24 09:30:04 -0500

    75

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ class Array:
             return 0
         ans = mn = mx = nums[0]
         for num in nums[1:]:
-            mn, mx = min(num, mn * num, mx * num), max(num, mn * num, mx * num)
+            mn, mx = min(num, mn*num, mx*num), max(num, mn*num, mx*num)
             ans = max(mx, ans)
         return ans
 
@@ -588,8 +588,44 @@ class Binary:
             ans[x] = ans[x & (x - 1)] + 1
         return ans
 
+    """# - Missing Number -
+    # https://leetcode.com/problems/missing-number/
+    Given an array nums containing n distinct numbers in the range [0, n],
+    return the only number in the range that is missing from the array.
 
-# - Missing Number - https://leetcode.com/problems/missing-number/
+    Example 1:
+
+    Input: nums = [3,0,1]
+    Output: 2
+    Explanation: n = 3 since there are 3 numbers, so all numbers are in the
+    range [0,3]. 2 is the missing number in the range since it does not
+    appear in nums.
+
+    Example 2:
+
+    Input: nums = [0,1]
+    Output: 2
+    Explanation: n = 2 since there are 2 numbers, so all numbers are in the
+    range [0,2]. 2 is the missing number in the range since it does not
+    appear in nums.
+
+    Example 3:
+
+    Input: nums = [9,6,4,2,3,5,7,0,1]
+    Output: 8
+    Explanation: n = 9 since there are 9 numbers, so all numbers are in the
+    range [0,9]. 8 is the missing number in the range since it does not
+    appear in nums.
+
+    Constraints:
+
+    n == nums.length
+    1 <= n <= 104
+    0 <= nums[i] <= n
+    All the numbers of nums are unique.
+
+    Follow up: Could you implement a solution using only O(1) extra space
+    complexity and O(n) runtime complexity?"""
 # - Reverse Bits - https://leetcode.com/problems/reverse-bits/
 
 
