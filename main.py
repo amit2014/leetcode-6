8505f00af3300eb29edce01465569c30aa5e5d87
yrom1 <ryanm.inbox@gmail.com>
2021-12-25 06:47:05 -0500

    75

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from functools import lru_cache
 from math import inf
 from typing import Dict, List, Union
@@ -808,8 +809,48 @@ class Binary:
         fn_amount = fn(amount)
         return fn_amount if isinstance(fn_amount, int) else -1
 
+    """# - Longest Increasing Subsequence -
+    # https://leetcode.com/problems/longest-increasing-subsequence/
+    Given an integer array nums, return the length of the longest strictly
+    increasing subsequence.
 
-# - Longest Increasing Subsequence - https://leetcode.com/problems/longest-increasing-subsequence/
+    A subsequence is a sequence that can be derived from an array by deleting
+    some or no elements without changing the order of the remaining elements.
+    For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+    Example 1:
+    Input: nums = [10,9,2,5,3,7,101,18]
+    Output: 4
+    Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+    the length is 4.
+
+    Example 2:
+    Input: nums = [0,1,0,3,2,3]
+    Output: 4
+
+    Example 3:
+    Input: nums = [7,7,7,7,7,7,7]
+    Output: 1
+
+    Constraints:
+
+    1 <= nums.length <= 2500
+    -104 <= nums[i] <= 104
+
+    Follow up: Can you come up with an algorithm that runs in O(n log(n)) time
+    complexity?"""
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        arr: List[int] = []
+        for num in nums:
+            i = bisect_left(arr, num)
+            if i == len(arr):
+                arr.append(num)
+            else:
+                arr[i] = num
+        return len(arr)
+
+
 # - Longest Common Subsequence -
 # - Word Break Problem - https://leetcode.com/problems/word-break/
 # - Combination Sum - https://leetcode.com/problems/combination-sum-iv/
