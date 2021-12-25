9d4f01023d0b2de912046cbf8712d7589efc6557
yrom1 <ryanm.inbox@gmail.com>
2021-12-25 04:45:50 -0500

    75

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
-from typing import Dict, List
-
+from typing import Dict, List, Union
+from math import inf
 
 class Array:
     """
@@ -759,7 +759,46 @@ class Binary:
             return fn(k - 1) + fn(k - 2)
         return fn(n)
 
-# - Coin Change - https://leetcode.com/problems/coin-change/
+    """
+    # - Coin Change -
+    # https://leetcode.com/problems/coin-change/
+    You are given an integer array coins representing coins of different
+    denominations and an integer amount representing a total amount of money.
+
+    Return the fewest number of coins that you need to make up that amount. If
+    that amount of money cannot be made up by any combination of the coins,
+    return -1.
+
+    You may assume that you have an infinite number of each kind of coin.
+
+    Example 1:
+    Input: coins = [1,2,5], amount = 11
+    Output: 3
+    Explanation: 11 = 5 + 5 + 1
+
+    Example 2:
+    Input: coins = [2], amount = 3
+    Output: -1
+
+    Example 3:
+    Input: coins = [1], amount = 0
+    Output: 0
+
+    Constraints:
+
+    1 <= coins.length <= 12
+    1 <= coins[i] <= 231 - 1
+    0 <= amount <= 104"""
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """O(S * n) time, O(S) space
+        where S is amount n is denomination count"""
+        @lru_cache(maxsize=None)
+        def fn(x: int) -> Union[float, int]:
+            if x == 0: return 0
+            if x < 0: return inf
+            return min(1 + fn(x - coin) for coin in coins)
+        return fn(amount) if fn(amount) < inf else -1
+
 # - Longest Increasing Subsequence - https://leetcode.com/problems/longest-increasing-subsequence/
 # - Longest Common Subsequence -
 # - Word Break Problem - https://leetcode.com/problems/word-break/
