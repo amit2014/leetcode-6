95d196105043896a0f77a934cd549c47ca266180
yrom1 <ryanm.inbox@gmail.com>
2021-12-27 11:26:36 -0500

    75

--- main.py
+++ main.py
@@ -998,8 +998,39 @@ class Binary:
 
         return fn(target)
 
+    """
+    # - House Robber -
+    # https://leetcode.com/problems/house-robber/
+    You are a professional robber planning to rob houses along a street. Each
+    house has a certain amount of money stashed, the only constraint stopping
+    you from robbing each of them is that adjacent houses have security system
+    connected and it will automatically contact the police if two adjacent
+    houses were broken into on the same night. Given a list of non-negative
+    integers representing the amount of money of each house, determine the
+    maximum amount of money you can rob tonight without alerting the police.
 
-# - House Robber - https://leetcode.com/problems/house-robber/
+    Example 1:
+    Input: nums = [1,2,3,1]
+    Output: 4
+    Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+    Total amount you can rob = 1 + 3 = 4.
+
+    Example 2:
+    Input: nums = [2,7,9,3,1]
+    Output: 12
+    Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house
+    5 (money = 1). Total amount you can rob = 2 + 9 + 1 = 12."""
+
+    def rob(self, nums: List[int]) -> int:
+
+        @lru_cache(maxsize=None)
+        def fn(i):
+            """Return the maximum amount of money after robbing ith house"""
+            if i < 0: return 0
+            return max(fn(i-1), fn(i-2) + nums[i])
+
+        return fn(len(nums)-1)
+
 # - House Robber II - https://leetcode.com/problems/house-robber-ii/
 # - Decode Ways - https://leetcode.com/problems/decode-ways/
 # - Unique Paths - https://leetcode.com/problems/unique-paths/
