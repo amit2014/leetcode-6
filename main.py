f2a1626a1e08319680453d4eef6cc2e9169d874b
yrom1 <ryanm.inbox@gmail.com>
2022-02-12 14:34:11 -0500

    75

--- main.py
+++ main.py
@@ -1405,7 +1405,41 @@ class Matrix:
 
 
 class String:
-    # - Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    """
+    # - Longest Substring Without Repeating Characters -
+    # https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    Given a string s, find the length of the longest substring without repeating
+    characters.
+
+    Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+    Example 2:
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+    Example 3:
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a
+    substring.
+    """
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp: Dict[str, int] = {}  # char -> highest_string_idx
+        ans = i = 0
+        for j in range(len(s)):
+            c = s[j]
+            if c in mp:
+                i = max(mp[c] + 1, i)
+            ans = max(ans, j - i + 1)
+            mp[c] = j
+        return ans
+
     # - Longest Repeating Character Replacement - https://leetcode.com/problems/longest-repeating-character-replacement/
     # - Minimum Window Substring - https://leetcode.com/problems/minimum-window-substring/
     # - Valid Anagram - https://leetcode.com/problems/valid-anagram/
