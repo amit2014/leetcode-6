7bd1f559d069ce5d53b8818d87bb15edffbb9f50
yrom1 <ryanm.inbox@gmail.com>
2022-02-16 15:00:14 -0500

    75

--- main.py
+++ main.py
@@ -1781,7 +1781,33 @@ class Tree:
 
         return fn(p, q)
 
-    # - Invert/Flip Binary Tree - https://leetcode.com/problems/invert-binary-tree/
+    """
+    # - Invert/Flip Binary Tree -
+    # https://leetcode.com/problems/invert-binary-tree/
+    Given the root of a binary tree, invert the tree, and return its root.
+
+    Example 1:
+    Input: root = [4,2,7,1,3,6,9]
+    Output: [4,7,2,9,6,3,1]
+
+    Example 2:
+        (2)                 (2)
+        / \        ->       / \
+     (1)   (3)           (3)   (1)
+    Input: root = [2,1,3]
+    Output: [2,3,1]
+
+    Example 3:
+    Input: root = []
+    Output: []
+    """
+
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
     # - Binary Tree Maximum Path Sum - https://leetcode.com/problems/binary-tree-maximum-path-sum/
     # - Binary Tree Level Order Traversal - https://leetcode.com/problems/binary-tree-level-order-traversal/
     # - Serialize and Deserialize Binary Tree - https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
