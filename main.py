b11b9c71f5f64e8be3d9d99fffaf2c237700bb2f
yrom1 <ryanm.inbox@gmail.com>
2022-02-16 14:48:35 -0500

    75

--- main.py
+++ main.py
@@ -1748,7 +1748,39 @@ class Tree:
 
         return fn(root)
 
-    # - Same Tree - https://leetcode.com/problems/same-tree/
+    """
+    # - Same Tree -
+    # https://leetcode.com/problems/same-tree/
+    Given the roots of two binary trees p and q, write a function to check if
+    they are the same or not.
+
+    Two binary trees are considered the same if they are structurally
+    identical, and the nodes have the same value.
+
+    Example 1:
+        (1)             (1)
+       /   \           /   \
+    (2)    (3)       (2)   (3)
+    Input: p = [1,2,3], q = [1,2,3]
+    Output: true
+
+    Example 2:
+    Input: p = [1,2], q = [1,null,2]
+    Output: false
+
+    Example 3:
+    Input: p = [1,2,1], q = [1,1,2]
+    Output: false
+    """
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def fn(p, q):
+            if not p or not q:
+                return p is q
+            return fn(p.left, q.left) and p.val == q.val and fn(p.right, q.right)
+
+        return fn(p, q)
+
     # - Invert/Flip Binary Tree - https://leetcode.com/problems/invert-binary-tree/
     # - Binary Tree Maximum Path Sum - https://leetcode.com/problems/binary-tree-maximum-path-sum/
     # - Binary Tree Level Order Traversal - https://leetcode.com/problems/binary-tree-level-order-traversal/
