5b802c1de44f66b55b49742a96cd190009125b6b
yrom1 <ryanm.inbox@gmail.com>
2022-02-17 04:57:23 -0500

    Testing WSL commit ( •_•)

--- leetcode.py
+++ leetcode.py
@@ -2126,7 +2126,32 @@ class Tree:
 
         return fn(root)
 
-    # - Kth Smallest Element in a BST - https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    r"""
+    # - Kth Smallest Element in a BST -
+    # https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+    Example 1:
+        3
+       / \
+      1   4
+       \
+        2
+    Input: root = [3,1,4,null,2], k = 1
+    Output: 1
+
+    Example 2:
+                5
+               / \
+              3   6
+             / \
+            2   4
+           /
+          1
+    Input: root = [5,3,6,2,4,null,null,1], k = 3
+    Output: 3
+    """
+
     # - Lowest Common Ancestor of BST - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
     # - Implement Trie (Prefix Tree) - https://leetcode.com/problems/implement-trie-prefix-tree/
     # - Add and Search Word - https://leetcode.com/problems/add-and-search-word-data-structure-design/
