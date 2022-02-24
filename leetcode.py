f2c037c0df50be3a4611b94069d88348a85cf48e
yrom1 <ryanm.inbox@gmail.com>
2022-02-24 01:32:13 -0500

    75

--- leetcode.py
+++ leetcode.py
@@ -1177,7 +1177,35 @@ class Graph:
 
     # - Course Schedule - https://leetcode.com/problems/course-schedule/
     # - Pacific Atlantic Water Flow - https://leetcode.com/problems/pacific-atlantic-water-flow/
-    # - Number of Islands - https://leetcode.com/problems/number-of-islands/
+    """
+    # - Number of Islands -
+    # https://leetcode.com/problems/number-of-islands/
+    Given an m x n 2D binary grid grid which represents a map of '1's (land)
+    and '0's (water), return the number of islands.
+
+    An island is surrounded by water and is formed by connecting adjacent lands
+    horizontally or vertically. You may assume all four edges of the grid are
+    all surrounded by water.
+
+    Example 1:
+    Input: grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+    ]
+    Output: 1
+
+    Example 2:
+    Input: grid = [
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+    ]
+    Output: 3
+    """
+
     # - Longest Consecutive Sequence - https://leetcode.com/problems/longest-consecutive-sequence/
     """
     # - Alien Dictionary (Leetcode Premium) -
