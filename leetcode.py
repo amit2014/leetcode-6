70fb46b0a497b3d74a4f4ae6de32d0ed04f7c8f5
yrom1 <ryanm.inbox@gmail.com>
2022-02-24 00:53:08 -0500

    75

--- leetcode.py
+++ leetcode.py
@@ -1250,14 +1250,17 @@ class Graph:
 
 class UnionFind:
     def __init__(self, n: int):
+        # O(n) space
         self.parent = [node for node in range(n)]
 
     def find(self, A: int):
+        # O(inverse Ackermann function) time
         while A != self.parent[A]:
             A = self.parent[A]
         return A
 
     def union(self, A: int, B: int) -> bool:
+        # O(inverse Ackermann function) time
         """True if a merge happened, False otherwise"""
         root_A = self.find(A)
         root_B = self.find(B)
@@ -1963,7 +1966,85 @@ class String:
                 ans += dp[i][j]
         return ans
 
-    # - Encode and Decode Strings (Leetcode Premium) - https://leetcode.com/problems/encode-and-decode-strings/
+    """
+    # - Encode and Decode Strings (Leetcode Premium) -
+    # https://leetcode.com/problems/encode-and-decode-strings/
+    Design an algorithm to encode a list of strings to a string. The encoded string is
+    then sent over the network and is decoded back to the original list of strings.
+
+    Machine 1 (sender) has the function:
+
+    string encode(vector<string> strs) {
+    // ... your code
+    return encoded_string;
+    }
+    Machine 2 (receiver) has the function:
+    vector<string> decode(string s) {
+    //... your code
+    return strs;
+    }
+    So Machine 1 does:
+
+    string encoded_string = encode(strs);
+    and Machine 2 does:
+
+    vector<string> strs2 = decode(encoded_string);
+    strs2 in Machine 2 should be the same as strs in Machine 1.
+
+    Implement the encode and decode methods.
+
+    You are not allowed to solve the problem using any serialize methods (such as eval).
+
+    Example 1:
+    Input: dummy_input = ["Hello","World"]
+    Output: ["Hello","World"]
+
+    Explanation:
+    Machine 1:
+    Codec encoder = new Codec();
+    String msg = encoder.encode(strs);
+    Machine 1 ---msg---> Machine 2
+    Machine 2:
+    Codec decoder = new Codec();
+    String[] strs = decoder.decode(msg);
+
+    Example 2:
+    Input: dummy_input = [""]
+    Output: [""]
+
+    NOTE strs[i] contains any possible characters out of 256 valid ASCII characters.
+    """
+
+    class Codec:
+        # simple non ascii solution
+        def encode(self, strs: List[str]) -> str:
+            # O(n) time, O(1) space
+            if len(strs) == 0:
+                return chr(258)
+            return chr(257).join(str_ for str_ in strs)
+
+        def decode(self, s: str) -> List[str]:
+            # O(n) time, O(n) space
+            if s == chr(258):
+                return []
+            return s.split(chr(257))
+
+    class Codec_:
+        # chuncked transfer encoding
+        # eg "4|abcd3|efg"
+        def encode(self, strs: List[str]) -> str:
+            # O(n) time, O(1) space
+            return "".join(f"{len(ss)}|{ss}" for ss in strs)
+
+        def decode(self, s: str) -> List[str]:
+            # O(n) time, O(n) space
+            ans = []
+            i = 0
+            while i < len(s):
+                ii = s.find("|", i)
+                i = ii + int(s[i:ii]) + 1
+                ans.append(s[ii + 1 : i])
+            return ans
 
     """
     # - Edit Distance -
@@ -1995,7 +2076,7 @@ class String:
     exention -> exection (replace 'n' with 'c')
     exection -> execution (insert 'u')
     """
-
+    # NOTE levenshtein distance, not in original 75 list
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
 
