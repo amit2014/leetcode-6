a64237bdcd388a4e065cd27e5fe674a9c46e7c92
yrom1 <ryanm.inbox@gmail.com>
2022-02-25 00:23:30 -0500

    Random question docstring selector

---
+++ random-question.py
@@ -0,0 +1,12 @@
+"""Get a random question's docstring, should work 95% of the time ;^)."""
+import random
+import tokenize
+
+with open("leetcode.py") as f:
+    questions = []
+    for info in tokenize.generate_tokens(f.readline):
+        a, b, c, d, e = info
+        if a == tokenize.STRING and len(b) > 30: # ;^)
+            questions.append(b)
+
+print(random.choice(questions))
