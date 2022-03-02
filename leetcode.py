7fdfa0010299b25ab48ec8aaf515270060e46265
yrom1 <ryanm.inbox@gmail.com>
2022-03-02 09:59:12 -0500

    Messy course schedule

--- leetcode.py
+++ leetcode.py
@@ -1113,6 +1113,11 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
+class GNode(object):
+    """  data structure represent a vertex in the graph."""
+    def __init__(self):
+        self.inDegrees = 0
+        self.outNodes = []
 
 class Graph:
     """
@@ -1175,8 +1180,74 @@ class Graph:
 
         return node and fn(node)
 
-    # - Course Schedule - https://leetcode.com/problems/course-schedule/
+    """
+    # - Course Schedule -
+    # https://leetcode.com/problems/course-schedule/
+    There are a total of numCourses courses you have to take, labeled from 0
+    to numCourses - 1. You are given an array prerequisites where
+    prerequisites[i] = [ai, bi] indicates that you must take course bi first
+    if you want to take course ai.
+
+    For example, the pair [0, 1], indicates that to take course 0 you have to
+    first take course 1. Return true if you can finish all courses.
+    Otherwise, return false.
+
+    Example 1:
+    Input: numCourses = 2, prerequisites = [[1,0]]
+    Output: true
+    Explanation: There are a total of 2 courses to take.
+    To take course 1 you should have finished course 0. So it is possible.
+
+    Example 2:
+    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    Output: false
+    Explanation: There are a total of 2 courses to take.
+    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+    # NOTE All the pairs prerequisites[i] are unique.
+    """
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # key: index of node; value: GNode
+        graph = defaultdict(GNode)
+
+        totalDeps = 0
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            graph[prevCourse].outNodes.append(nextCourse)
+            graph[nextCourse].inDegrees += 1
+            totalDeps += 1
+
+        # we start from courses that have no prerequisites.
+        # we could use either set, stack or queue to keep track of courses with no dependence.
+        nodepCourses = deque()
+        for index, node in graph.items():
+            if node.inDegrees == 0:
+                nodepCourses.append(index)
+
+        removedEdges = 0
+        while nodepCourses:
+            # pop out course without dependency
+            course = nodepCourses.pop()
+
+            # remove its outgoing edges one by one
+            for nextCourse in graph[course].outNodes:
+                graph[nextCourse].inDegrees -= 1
+                removedEdges += 1
+                # while removing edges, we might discover new courses with prerequisites removed, i.e. new courses without prerequisites.
+                if graph[nextCourse].inDegrees == 0:
+                    nodepCourses.append(nextCourse)
+
+        if removedEdges == totalDeps:
+            return True
+        else:
+            # if there are still some edges left, then there exist some cycles
+            # Due to the dead-lock (dependencies), we cannot remove the cyclic edges
+            return False
+
     # - Pacific Atlantic Water Flow - https://leetcode.com/problems/pacific-atlantic-water-flow/
+
+
     """
     # - Number of Islands -
     # https://leetcode.com/problems/number-of-islands/
@@ -1309,7 +1380,7 @@ class UnionFind:
         self.parent = [node for node in range(n)]
         self.size = [1] * n
 
-    def find(self, A: int):
+    def find(self, A: int) -> int:
         # O(inverse Ackermann function) time
         while self.parent[A] != A:
             A, self.parent[A] = self.parent[A], self.parent[self.parent[A]]
