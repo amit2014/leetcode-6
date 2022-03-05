30e46598d6fb5bac402734ce65d6e54c979d5096
yrom1 <ryanm.inbox@gmail.com>
2022-03-05 17:59:13 -0500

    SELECT 'em all, Leetcodemon!

--- leetcode.sql
+++ leetcode.sql
@@ -818,7 +818,44 @@ GROUP BY customer_id
 HAVING SUM(IF(LEFT(order_date, 7) = '2020-06', quantity, 0) * price) >= 100
     AND SUM(IF(LEFT(order_date, 7) = '2020-07', quantity, 0) * price) >= 100
 
+/*
 627. Swap Salary (Easy)
+--https://leetcode.com/problems/swap-salary
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+Output:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
+Explanation:
+(1, A) and (3, C) were changed from 'm' to 'f'.
+(2, B) and (4, D) were changed from 'f' to 'm'.
+*/
+UPDATE Salary SET sex = CASE sex WHEN 'm' THEN 'f' ELSE 'm' END;
+UPDATE Salary SET sex = IF(sex='m', 'f', 'm');
+
+
 569. Median Employee Salary (Hard)
 1164. Product Price at a Given Date (Medium)
 -- https://leetcode.com/problems/product-price-at-a-given-date
