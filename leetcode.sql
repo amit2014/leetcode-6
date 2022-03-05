b4fc645538ad77e1d583a66e23629c82b3480f62
yrom1 <ryanm.inbox@gmail.com>
2022-03-05 17:56:31 -0500

    SELECT 'em all, Leetcodemon!

--- leetcode.sql
+++ leetcode.sql
@@ -757,8 +757,67 @@ From 2019-01-06 to 2019-01-06 all tasks succeeded and the system state was "succ
 
 
 615. Average Salary: Departments VS Company (Hard)
+
+/*
 1511. Customer Order Frequency (Easy)
 -- https://leetcode.com/problems/customer-order-frequency
+Write an SQL query to report the customer_id and customer_name of customers who have spent at least $100 in each month of June and July 2020.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Customers table:
++--------------+-----------+-------------+
+| customer_id  | name      | country     |
++--------------+-----------+-------------+
+| 1            | Winston   | USA         |
+| 2            | Jonathan  | Peru        |
+| 3            | Moustafa  | Egypt       |
++--------------+-----------+-------------+
+Product table:
++--------------+-------------+-------------+
+| product_id   | description | price       |
++--------------+-------------+-------------+
+| 10           | LC Phone    | 300         |
+| 20           | LC T-Shirt  | 10          |
+| 30           | LC Book     | 45          |
+| 40           | LC Keychain | 2           |
++--------------+-------------+-------------+
+Orders table:
++--------------+-------------+-------------+-------------+-----------+
+| order_id     | customer_id | product_id  | order_date  | quantity  |
++--------------+-------------+-------------+-------------+-----------+
+| 1            | 1           | 10          | 2020-06-10  | 1         |
+| 2            | 1           | 20          | 2020-07-01  | 1         |
+| 3            | 1           | 30          | 2020-07-08  | 2         |
+| 4            | 2           | 10          | 2020-06-15  | 2         |
+| 5            | 2           | 40          | 2020-07-01  | 10        |
+| 6            | 3           | 20          | 2020-06-24  | 2         |
+| 7            | 3           | 30          | 2020-06-25  | 2         |
+| 9            | 3           | 30          | 2020-05-08  | 3         |
++--------------+-------------+-------------+-------------+-----------+
+Output:
++--------------+------------+
+| customer_id  | name       |
++--------------+------------+
+| 1            | Winston    |
++--------------+------------+
+Explanation:
+Winston spent $300 (300 * 1) in June and $100 ( 10 * 1 + 45 * 2) in July 2020.
+Jonathan spent $600 (300 * 2) in June and $20 ( 2 * 10) in July 2020.
+Moustafa spent $110 (10 * 2 + 45 * 2) in June and $0 in July 2020.
+*/
+SELECT customer_id, name
+FROM Customers JOIN Orders USING(customer_id)
+    JOIN Product USING(product_id)
+GROUP BY customer_id
+HAVING SUM(IF(LEFT(order_date, 7) = '2020-06', quantity, 0) * price) >= 100
+    AND SUM(IF(LEFT(order_date, 7) = '2020-07', quantity, 0) * price) >= 100
+
 627. Swap Salary (Easy)
 569. Median Employee Salary (Hard)
 1164. Product Price at a Given Date (Medium)
