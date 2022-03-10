4f08565c0b6d65a494e3b3ea325fc015eeb98c23
yrom1 <ryanm.inbox@gmail.com>
2022-03-10 18:03:57 -0500

    75

--- leetcode.py
+++ leetcode.py
@@ -1984,8 +1984,52 @@ class LinkedList:
 
 
 class Matrix:
-    ...
-    # - Set Matrix Zeroes - https://leetcode.com/problems/set-matrix-zeroes/
+    """
+    # - Set Matrix Zeroes -
+    # https://leetcode.com/problems/set-matrix-zeroes/
+    Given an m x n integer matrix matrix, if an element is 0, set its entire row
+    and column to 0's.
+
+    You must do it in place.
+
+    Example 1:
+    1 1 1    1 0 1
+    1 0 1 -> 0 0 0
+    1 1 1    1 0 1
+    Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+    Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+    Example 2:
+    0 1 2 0    0 0 0 0
+    3 4 5 2 -> 0 4 5 0
+    1 3 1 5    0 3 1 0
+    Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+    """
+
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        # O(mn) time, O(1) space
+        num_cols, num_rows = len(matrix[0]), len(matrix)
+        r1 = any(matrix[0][j] == 0 for j in range(num_cols))
+        c1 = any(matrix[i][0] == 0 for i in range(num_rows))
+        for i in range(1, num_rows):
+            for j in range(1, num_cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+
+        for i in range(1, num_rows):
+            for j in range(1, num_cols):
+                if matrix[i][0] * matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if r1:
+            for i in range(num_cols):
+                matrix[0][i] = 0
+
+        if c1:
+            for j in range(num_rows):
+                matrix[j][0] = 0
+
     # - Spiral Matrix - https://leetcode.com/problems/spiral-matrix/
     # - Rotate Image - https://leetcode.com/problems/rotate-image/
     # - Word Search - https://leetcode.com/problems/word-search/
