bf38dc4040e67c742c5713cb6c253a243acfc262
yrom1 <ryanm.inbox@gmail.com>
2022-03-11 03:01:51 -0500

    75

--- leetcode.py
+++ leetcode.py
@@ -3204,6 +3204,36 @@ class Tree:
                 node = node.children[i]
             return True
 
+    class Trie__:
+        # NOTE all inputs assumed to consist of lowercase letters a-z.
+        # NOTE all inputs are guaranteed to be non-empty strings.
+        def __init__(self):
+            # TODO recursive type pyright
+            # Node = Dict[str, "Node"]
+            self.root = {}
+
+        def insert(self, word: str) -> None:
+            node = self.root
+            for char in word:
+                node = node.setdefault(char, {})
+            node["$"] = word
+
+        def search(self, word: str) -> bool:
+            node = self.root
+            for char in word:
+                if char not in node:
+                    return False
+                node = node[char]
+            return bool(node.get("$"))  # TODO this passes without the bool???????
+
+        def startsWith(self, prefix: str) -> bool:
+            node = self.root
+            for char in prefix:
+                if char not in node:
+                    return False
+                node = node[char]
+            return True
+
     """
     # - Add and Search Word -
     # https://leetcode.com/problems/add-and-search-word-data-structure-design/
@@ -3246,16 +3276,90 @@ class Tree:
         def search(self, word: str) -> bool:
             ...
 
-    # - Word Search II - https://leetcode.com/problems/word-search-ii/
+    """
+    # - Word Search II -
+    # https://leetcode.com/problems/word-search-ii/
+    Given an m x n board of characters and a list of strings words, return all
+    words on the board.
+
+    Each word must be constructed from letters of sequentially adjacent cells,
+    where adjacent cells are horizontally or vertically neighboring. The same
+    letter cell may not be used more than once in a word.
+
+    Example 1:
+    Input: board = [["o","a","a","n"],
+                    ["e","t","a","e"],
+                    ["i","h","k","r"],
+                    ["i","f","l","v"]],
+    words = ["oath","pea","eat","rain"]
+    Output: ["eat","oath"]
+
+    Example 2:
+    Input: board = [["a","b"],
+                    ["c","d"]],
+    words = ["abcb"]
+    Output: []
+    """
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # TODO clean up, lots of solutions time out now on new tests
+        WORD_KEY = "$"
+
+        trie = {}  # type:ignore
+        for word in words:
+            node = trie
+            for letter in word:
+                # retrieve the next node; If not found, create a empty node.
+                node = node.setdefault(letter, {})
+            # mark the existence of a word in trie node
+            node[WORD_KEY] = word
+
+        rowNum = len(board)
+        colNum = len(board[0])
+
+        matchedWords = []
+
+        def backtracking(row, col, parent):
+
+            letter = board[row][col]
+            currNode = parent[letter]
+
+            # check if we find a match of word
+            word_match = currNode.pop(WORD_KEY, False)
+            if word_match:
+                # also we removed the matched word to avoid duplicates,
+                #   as well as avoiding using set() for results.
+                matchedWords.append(word_match)
+
+            # Before the EXPLORATION, mark the cell as visited
+            board[row][col] = "#"
+
+            # Explore the neighbors in 4 directions, i.e. up, right, down, left
+            for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                newRow, newCol = row + rowOffset, col + colOffset
+                if newRow < 0 or newRow >= rowNum or newCol < 0 or newCol >= colNum:
+                    continue
+                if not board[newRow][newCol] in currNode:
+                    continue
+                backtracking(newRow, newCol, currNode)
+
+            # End of EXPLORATION, we restore the cell
+            board[row][col] = letter
+
+            # Optimization: incrementally remove the matched leaf node in Trie.
+            if not currNode:
+                parent.pop(letter)
+
+        for row in range(rowNum):
+            for col in range(colNum):
+                # starting from each of the cells
+                if board[row][col] in trie:
+                    backtracking(row, col, trie)
+
+        return matchedWords
 
 
 class Heap:
-    """# - Merge K Sorted Lists -
-    # https://leetcode.com/problems/merge-k-sorted-lists/"""
-
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        ...  # see class LinkedList
-
     """
     # - Top K Frequent Elements -
     # https://leetcode.com/problems/top-k-frequent-elements/
