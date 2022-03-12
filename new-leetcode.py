9d7cacd59102297034243ebf01c08e59827e06a6
yrom1 <ryanm.inbox@gmail.com>
2022-03-12 15:55:44 -0500

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -9,6 +9,22 @@ from itertools import chain, product
 from math import comb, inf
 from typing import Callable, Deque, Dict, Final, List, Optional, Set, Tuple, Union
 
+#########################################################################################
+# - Shared Classes -
+
+
+class ListNode:
+    def __init__(
+        self, key: int = 0, val: int = 0, prev: ListNode = None, next: ListNode = None
+    ):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
+#########################################################################################
+
 
 class _1:
     """
@@ -50,9 +66,81 @@ class _146:
     """
     # - LRU Cache -
     # https://leetcode.com/problems/lru-cache/
+    Design a data structure that follows the constraints of a Least Recently
+    Used (LRU) cache.
+
+    Implement the LRUCache class:
+        - `LRUCache(int capacity)` Initialize the LRU cache with positive
+            size capacity.
+        - `int get(int key)` Return the value of the key if the key exists,
+            otherwise return -1.
+        - `void put(int key, int value)` Update the value of the key if the
+            key exists. Otherwise, add the key-value pair to the cache. If
+            the number of keys exceeds the capacity from this operation,
+            evict the least recently used key.
+
+    The functions get and put must each run in O(1) average time complexity.
+
+    Example 1:
+    Input
+    ["LRUCache", "put" , "put" , "get", "put" , "get", "put" , "get", "get", "get"]
+    [[2]       , [1, 1], [2, 2], [1]  , [3, 3], [2]  , [4, 4], [1]  , [3]  , [4]  ]
+    Output
+    [null      , null  , null  , 1    , null  , -1   , null  , -1   , 3    , 4    ]
+
+    Explanation
+    LRUCache lRUCache = new LRUCache(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1);    // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2);    // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1);    // return -1 (not found)
+    lRUCache.get(3);    // return 3
+    lRUCache.get(4);    // return 4
     """
 
-    ...
+    class LRUCache:
+        def __init__(self, capacity: int):
+            """O(n) space"""
+            assert capacity > 0
+            self.capacity = capacity
+            self.mp: Dict[int, ListNode] = {}  # hash table
+            self.head = ListNode()  # doubly linked list
+            self.tail = ListNode()
+            self.head.next = self.tail
+            self.tail.prev = self.head
+
+        def _delete(self, key: int) -> int:
+            node = self.mp.pop(key)
+            assert node.prev is not None and node.next is not None
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            return node.val
+
+        def _insert(self, key: int, value: int) -> None:
+            node = ListNode(key, value, self.tail.prev, self.tail)
+            assert node.prev is not None and node.next is not None
+            self.tail.prev.next = self.tail.prev = node  # insert at tail
+            self.mp[key] = node
+
+        def get(self, key: int) -> int:
+            """O(1) time"""
+            if key not in self.mp:
+                return -1
+            value = self._delete(key)
+            self._insert(key, value)
+            return value
+
+        def put(self, key: int, value: int) -> None:
+            """O(1) time"""
+            assert self.head.next is not None
+            if key in self.mp:
+                self._delete(key)
+            self._insert(key, value)
+            if len(self.mp) > self.capacity:
+                self._delete(self.head.next.key)
 
 
 class _56:
