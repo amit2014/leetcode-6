25f5a3f4cc286accd5597b55254c1f764888fe5a
yrom1 <ryanm.inbox@gmail.com>
2022-03-12 20:16:31 -0500

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -175,9 +175,54 @@ class _423:
     """
     # - Reconstruct Original Digits from English -
     # https://leetcode.com/problems/reconstruct-original-digits-from-english/
+    Given a string s containing an out-of-order English representation of digits
+    0-9, return the digits in ascending order.
+
+    Example 1:
+    Input: s = "owoztneoer"
+    Output: "012"
+
+    Example 2:
+    Input: s = "fviefuro"
+    Output: "45"
+
+    NOTE s[i] is one of the characters ["e","g","f","i","h","o","n","s","r","u",
+    "t","w","v","x","z"].
+    NOTE s is guaranteed to be valid.
     """
 
-    ...
+    # TODO what on earth is this question asking? here's leetcodes solution:
+
+    def originalDigits(self, s: str) -> str:
+        # building hashmap letter -> its frequency
+        count = Counter(s)
+
+        # building hashmap digit -> its frequency
+        out = {}
+        # letter "z" is present only in "zero"
+        out["0"] = count["z"]
+        # letter "w" is present only in "two"
+        out["2"] = count["w"]
+        # letter "u" is present only in "four"
+        out["4"] = count["u"]
+        # letter "x" is present only in "six"
+        out["6"] = count["x"]
+        # letter "g" is present only in "eight"
+        out["8"] = count["g"]
+        # letter "h" is present only in "three" and "eight"
+        out["3"] = count["h"] - out["8"]
+        # letter "f" is present only in "five" and "four"
+        out["5"] = count["f"] - out["4"]
+        # letter "s" is present only in "seven" and "six"
+        out["7"] = count["s"] - out["6"]
+        # letter "i" is present in "nine", "five", "six", and "eight"
+        out["9"] = count["i"] - out["5"] - out["6"] - out["8"]
+        # letter "n" is present in "one", "nine", and "seven"
+        out["1"] = count["n"] - out["7"] - 2 * out["9"]
+
+        # building output string
+        output = [key * out[key] for key in sorted(out.keys())]
+        return "".join(output)
 
 
 class _42:
