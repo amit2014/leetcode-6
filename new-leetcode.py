e30138b0ac266a4d5a74a5d3c8240693390a585f
yrom1 <ryanm.inbox@gmail.com>
2022-03-12 21:07:46 -0500

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -191,47 +191,57 @@ class _423:
     NOTE s is guaranteed to be valid.
     """
 
-    # TODO what on earth is this question asking? here's leetcodes solution:
-
     def originalDigits(self, s: str) -> str:
-        # building hashmap letter -> its frequency
-        count = Counter(s)
-
-        # building hashmap digit -> its frequency
-        out = {}
+        count: Dict[str, int] = Counter(s)
+        out: Dict[int, int] = {}
         # letter "z" is present only in "zero"
-        out["0"] = count["z"]
+        out[0] = count["z"]
         # letter "w" is present only in "two"
-        out["2"] = count["w"]
+        out[2] = count["w"]
         # letter "u" is present only in "four"
-        out["4"] = count["u"]
+        out[4] = count["u"]
         # letter "x" is present only in "six"
-        out["6"] = count["x"]
+        out[6] = count["x"]
         # letter "g" is present only in "eight"
-        out["8"] = count["g"]
+        out[8] = count["g"]
         # letter "h" is present only in "three" and "eight"
-        out["3"] = count["h"] - out["8"]
+        out[3] = count["h"] - out[8]
         # letter "f" is present only in "five" and "four"
-        out["5"] = count["f"] - out["4"]
+        out[5] = count["f"] - out[4]
         # letter "s" is present only in "seven" and "six"
-        out["7"] = count["s"] - out["6"]
+        out[7] = count["s"] - out[6]
         # letter "i" is present in "nine", "five", "six", and "eight"
-        out["9"] = count["i"] - out["5"] - out["6"] - out["8"]
+        out[9] = count["i"] - out[5] - out[6] - out[8]
         # letter "n" is present in "one", "nine", and "seven"
-        out["1"] = count["n"] - out["7"] - 2 * out["9"]
-
-        # building output string
-        output = [key * out[key] for key in sorted(out.keys())]
-        return "".join(output)
+        out[1] = count["n"] - out[7] - 2 * out[9]
+        return "".join([str(key) * out[key] for key in sorted(out.keys())])
 
 
 class _42:
     """
     # - Trapping Rain Water -
     # https://leetcode.com/problems/trapping-rain-water/
+    Given n non-negative integers representing an elevation map where the width of
+    each bar is 1, compute how much water it can trap after raining.
+
+    Example 1:
+    3|       X
+    2|   X   XX X
+    1|_X_XX_XXXXXX
+
+    Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    Output: 6
+    Explanation: The above elevation map (black section) is represented by array
+    [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section)
+    are being trapped.
+
+    Example 2:
+    Input: height = [4,2,0,3,2,5]
+    Output: 9
     """
 
-    ...
+    def trap(self, height: List[int]) -> int:
+        ...
 
 
 class _200:
