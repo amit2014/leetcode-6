bb4bdcc1d41fa678002d3a85325fa3e2a9b8c6a2
yrom1 <ryanm.inbox@gmail.com>
2022-03-13 03:59:08 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -961,9 +961,44 @@ class _704:
     """
     # - Binary Search -
     # https://leetcode.com/problems/binary-search/
+    Given an array of integers nums which is sorted in ascending order, and an
+    integer target, write a function to search target in nums. If target
+    exists, then return its index. Otherwise, return -1.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Example 1:
+    Input: nums = [-1,0,3,5,9,12], target = 9
+    Output: 4
+    Explanation: 9 exists in nums and its index is 4
+
+    Example 2:
+    Input: nums = [-1,0,3,5,9,12], target = 2
+    Output: -1
+    Explanation: 2 does not exist in nums so return -1
+
+    NOTE All the integers in nums are unique.
     """
 
-    ...
+    # This doesn't matter for Python, but when you do it in C++ it will:
+    # https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            pivot = (l + r) // 2
+            if nums[pivot] == target:
+                return pivot
+
+            if target < nums[pivot]:
+                r = pivot - 1
+            else:
+                l = pivot + 1
+        return -1
+
+    def search_(self, nums: List[int], target: int) -> int:
+        index = bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
 
 
 class _9:
