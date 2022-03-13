d596b75e14778a04e58bb9377386848c2c547f15
yrom1 <ryanm.inbox@gmail.com>
2022-03-12 21:53:26 -0500

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -241,7 +241,23 @@ class _42:
     """
 
     def trap(self, height: List[int]) -> int:
-        ...
+        l, r = 0, len(height) - 1
+        l_max = r_max = area = 0
+
+        while l < r:
+            if height[l] < height[r]:
+                if height[l] < l_max:
+                    area += l_max - height[l]
+                else:
+                    l_max = height[l]
+                l += 1
+            else:
+                if height[r] < r_max:
+                    area += r_max - height[r]
+                else:
+                    r_max = height[r]
+                r -= 1
+        return area
 
 
 class _200:
