174df59188cb2405d25f1d00528bea8c700cd53b
yrom1 <ryanm.inbox@gmail.com>
2022-03-14 02:45:10 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -477,9 +477,48 @@ class _1249:
     """
     # - Minimum Remove to Make Valid Parentheses -
     # https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+    Given a string s of '(' , ')' and lowercase English characters.
+
+    Your task is to remove the minimum number of parentheses ('(' or ')', in
+    any positions) so that the resulting parentheses string is valid and
+    return any valid string.
+
+    Formally, a parentheses string is valid if and only if:
+        - It is the empty string, contains only lowercase characters, or
+        - It can be written as AB (A concatenated with B), where A and B are
+          valid strings, or
+        - It can be written as (A), where A is a valid string.
+
+    Example 1:
+    Input: s = "lee(t(c)o)de)"
+    Output:    "lee(t(c)o)de"
+    Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+    Example 2:
+    Input: s = "a)b(c)d"
+    Output:    "ab(c)d"
+
+    Example 3:
+    Input: s = "))(("
+    Output:    ""
+    Explanation: An empty string is also valid.
     """
 
-    ...
+    def minRemoveToMakeValid(self, s: str) -> str:
+        """O(n) time and space"""
+        s = list(s)
+        stack: List[int] = []
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif c == ")":
+                if stack:  # matching
+                    stack.pop()
+                else:  # extra
+                    s[i] = ""
+        while stack:
+            s[stack.pop()] = ""
+        return "".join(s)
 
 
 class _20:
@@ -508,6 +547,7 @@ class _20:
     """
 
     def isValid(self, s: str) -> bool:
+        """O(n) time and space"""
         stack: List[str] = []
         match = {"(": ")", "[": "]", "{": "}"}
         for x in s:
