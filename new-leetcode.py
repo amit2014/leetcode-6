9cf799cd1c586b6a545eac5d7715d6b9a8cd39cc
yrom1 <ryanm.inbox@gmail.com>
2022-03-14 00:47:20 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -839,9 +839,32 @@ class _50:
     """
     # - Pow(x, n) -
     # https://leetcode.com/problems/powx-n/
+    Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+
+    Example 1:
+    Input: x = 2.00000, n = 10
+    Output: 1024.00000
+
+    Example 2:
+    Input: x = 2.10000, n = 3
+    Output: 9.26100
+
+    Example 3:
+    Input: x = 2.00000, n = -2
+    Output: 0.25000
+    Explanation: 2-2 = 1/22 = 1/4 = 0.25
     """
 
-    ...
+    def myPow(self, x: float, n: int) -> float:
+        """O(logn) time, O(1) space"""
+        if n < 0:
+            x, n = 1 / x, -n
+        ans = 1.0
+        while n:
+            if n & 1:
+                ans *= x
+            x, n = x * x, n // 2
+        return ans
 
 
 class _79:
