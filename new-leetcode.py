aa2c59f606b8a744c6731b43c32aace64a5b4ad2
yrom1 <ryanm.inbox@gmail.com>
2022-03-14 21:46:35 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -715,9 +715,54 @@ class _815:
     """
     # - Bus Routes -
     # https://leetcode.com/problems/bus-routes/
+    You are given an array routes representing bus routes where routes[i] is a
+    bus route that the ith bus repeats forever. For example, if
+    routes[0] = [1, 5, 7], this means that the 0th bus travels in the sequence
+    1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever. You will start at the bus
+    stop source (You are not on any bus initially), and you want to go to the
+    bus stop target. You can travel between bus stops by buses only. Return the
+    least number of buses you must take to travel from source to target. Return
+    -1 if it is not possible.
+
+    Example 1:
+    Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+    Output: 2
+    Explanation: The best strategy is take the first bus to the bus stop 7,
+                    then take the second bus to the bus stop 6.
+
+    Example 2:
+    Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+    Output: -1
+
+    NOTE All the values of routes[i] are unique.
     """
 
-    ...
+    def numBusesToDestination(
+        self, routes: List[List[int]], source: int, target: int
+    ) -> int:
+        if source == target:
+            return 0
+
+        mp: Dict[int, Set[int]] = defaultdict(set)  # {stop:set(bus#)}
+        for bus, stops in enumerate(routes):
+            for stop in stops:
+                mp[stop].add(bus)
+
+        queue = deque([(source, 0)])  # deque([(stop,bus_count)])
+        visited_stops: Set[int] = set()
+        visited_buses: Set[int] = set()
+        while queue:
+            stop, count = queue.popleft()
+            if stop == target:
+                return count
+            for bus in mp[stop]:
+                if bus not in visited_buses:  # prevent loop
+                    visited_buses.add(bus)
+                    for stop in routes[bus]:
+                        if stop not in visited_stops:  # prevent loop again
+                            visited_stops.add(stop)
+                            queue.append((stop, count + 1))
+        return -1
 
 
 class _588:
