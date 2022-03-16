84b79da1464a6cd6f0d9947ea1437074c5f9f46e
yrom1 <ryanm.inbox@gmail.com>
2022-03-16 15:44:08 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -7,7 +7,7 @@ from bisect import bisect_left
 from collections import Counter, defaultdict, deque
 from functools import lru_cache, reduce
 from heapq import heapify, heappop, heappush, nlargest
-from itertools import chain, product, zip_longest
+from itertools import chain, groupby, product, zip_longest
 from math import comb, inf
 from typing import Callable, Deque, Dict, Final, List, Optional, Set, Tuple, Union
 
@@ -1392,9 +1392,33 @@ class _696:
     """
     # - Count Binary Substrings -
     # https://leetcode.com/problems/count-binary-substrings/
+    Give a binary string s, return the number of non-empty substrings that have
+    the same number of 0's and 1's, and all the 0's and all the 1's in these
+    substrings are grouped consecutively.
+
+    Substrings that occur multiple times are counted the number of times they occur.
+
+    Example 1:
+    Input: s = "00110011"
+    Output: 6
+    Explanation: There are 6 substrings that have equal number of consecutive 1's
+    and 0's: "0011", "01", "1100", "10", "0011", and "01".
+    Notice that some of these substrings repeat and are counted the number of
+    times they occur.
+    Also, "00110011" is not a valid substring because all the 0's (and 1's) are
+    not grouped together.
+
+    Example 2:
+    Input: s = "10101"
+    Output: 4
+    Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal
+    number of consecutive 1's and 0's.
     """
 
-    ...
+    def countBinarySubstrings(self, s: str) -> int:
+        """O(n) time, O(n) space"""
+        groups = [len(list(values)) for _, values in groupby(s)]
+        return sum(min(a, b) for a, b in zip(groups, groups[1:]))
 
 
 class _17:
