cdfc1f1c07923912ef0855b244dd0f09a67709b8
yrom1 <ryanm.inbox@gmail.com>
2022-03-17 02:23:28 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -2388,9 +2388,108 @@ class _716:
     """
     # - Max Stack -
     # https://leetcode.com/problems/max-stack/
+    Design a max stack data structure that supports the stack operations and
+    supports finding the stack's maximum element.
+
+    Implement the MaxStack class:
+    * MaxStack() Initializes the stack object.
+    * void push(int x) Pushes element x onto the stack.
+    * int pop() Removes the element on top of the stack and returns it.
+    * int top() Gets the element on the top of the stack without removing it.
+    * int peekMax() Retrieves the maximum element in the stack without removing it.
+    * int popMax() Retrieves the maximum element in the stack and removes it. If
+    there is more than one maximum element, only remove the top-most one.
+
+    Example 1:
+    Input:
+    ["MaxStack","push","push","push","top","popMax","top","peekMax","pop","top"]
+    [[]        , [5]  , [1]  , [5]  , []  , []     , []  , []      , []  , []]
+    Output:
+    [null      , null , null , null , 5   , 5      , 1   , 5       , 1   , 5]
+    Explanation
+    MaxStack stk = new MaxStack();
+    stk.push(5);   // [5] the top of the stack and the maximum number is 5.
+    stk.push(1);   // [5, 1] the top of the stack is 1, but the maximum is 5.
+    stk.push(5);   // [5, 1, 5] the top of the stack is 5, which is also the
+                      maximum, because it is the top most one.
+    stk.top();     // return 5, [5, 1, 5] the stack did not change.
+    stk.popMax();  // return 5, [5, 1] the stack is changed now, and the top is
+                      different from the max.
+    stk.top();     // return 1, [5, 1] the stack did not change.
+    stk.peekMax(); // return 5, [5, 1] the stack did not change.
+    stk.pop();     // return 1, [5] the top of the stack and the max element is
+                      now 5.
+    stk.top();     // return 5, [5] the stack did not change.
+
+    Constraints:
+    NOTE There will be at least one element in the stack when pop, top, peekMax, or
+    popMax is called.
     """
 
-    ...
+    class MaxStack(list):
+        """O(n) time popmax, O(1) time others, O(n) space"""
+
+        def push(self, x):
+            mx = max(x, self[-1][1] if self else -inf)
+            self.append((x, mx))
+
+        def pop(self):
+            return list.pop(self)[0]  # self.pop()[0]
+
+        def top(self):
+            return self[-1][0]
+
+        def peekMax(self):
+            return self[-1][1]
+
+        def popMax(self):
+            mx = self[-1][1]
+            b = []
+            while self[-1][0] != mx:
+                b.append(self.pop())
+
+            self.pop()
+            for item in reversed(b):
+                self.push(item)
+            return mx
+
+    # class MaxStack:
+
+    #     def __init__(self):
+    #         """
+    #         initialize your data structure here.
+    #         """
+    #         self.k = 0              # key
+    #         self.pq = []            # priority queue
+    #         self.od = OrderedDict() # ordered dict
+
+    #     def push(self, x: int) -> None:
+    #         heappush(self.pq, (-x, self.k))
+    #         self.od[self.k] = x
+    #         self.k -= 1
+
+    #     def pop(self) -> int:
+    #         k, x = self.od.popitem(last=True)
+    #         return x
+
+    #     def top(self) -> int:
+    #         k, x = self.od.popitem(last=True)
+    #         self.od[k] = x
+    #         return x
+
+    #     def peekMax(self) -> int:
+    #         while self.pq:
+    #             x, k = heappop(self.pq)
+    #             if k in self.od: break
+    #         heappush(self.pq, (x, k))
+    #         return -x
+
+    #     def popMax(self) -> int:
+    #         while self.pq:
+    #             x, k = heappop(self.pq)
+    #             if k in self.od: break
+    #         self.od.pop(k)
+    #         return -x
 
 
 class _453:
