8157feb0fa1f94c020f10705218e9dfe453f49b8
yrom1 <ryanm.inbox@gmail.com>
2022-03-16 23:54:31 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -1232,9 +1232,72 @@ class _1570:
     """
     # - Dot Product of Two Sparse Vectors -
     # https://leetcode.com/problems/dot-product-of-two-sparse-vectors/
+    Given two sparse vectors, compute their dot product.
+
+    Implement class SparseVector:
+        - SparseVector(nums) Initializes the object with the vector nums
+        - dotProduct(vec) Compute the dot product between the instance of
+        SparseVector and vec
+
+    A sparse vector is a vector that has mostly zero values, you should store the
+    sparse vector efficiently and compute the dot product between two SparseVector.
+
+    Follow up: What if only one of the vectors is sparse?
+
+    Example 1:
+    Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+    Output: 8
+    Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+    v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+
+    Example 2:
+    Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+    Output: 0
+    Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+    v1.dotProduct(v2) = 0*0 + 1*0 + 0*0 + 0*0 + 0*2 = 0
+
+    Example 3:
+    Input: nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+    Output: 6
     """
 
-    ...
+    class SparseVector:
+        """Index-value pair method: O(n) time, O(L) space
+        n being the len(nums), L being the number of non-zero elements"""
+
+        def __init__(self, nums: List[int]):
+            self.pairs = []
+            for i, num in enumerate(nums):
+                if num != 0:
+                    self.pairs.append([i, num])
+
+        def dotProduct(self, vec: _1570.SparseVector) -> int:
+            ans = 0
+            p, q = 0, 0
+
+            while p < len(self.pairs) and q < len(vec.pairs):
+                if self.pairs[p][0] == vec.pairs[q][0]:
+                    ans += self.pairs[p][1] * vec.pairs[q][1]
+                    p += 1
+                    q += 1
+                elif self.pairs[p][0] < vec.pairs[q][0]:
+                    p += 1
+                else:
+                    q += 1
+
+            return ans
+
+    class SparseVector_:
+        """Hash set method: O(n) time, O(L) space
+        n being the len(nums), L being the number of non-zero elements"""
+
+        def __init__(self, nums: List[int]):
+            self.m = {i: x for i, x in enumerate(nums) if x}
+
+        def dotProduct(self, vec: _1570.SparseVector_) -> int:
+            return sum(
+                [x * y for i, x in self.m.items() for j, y in vec.m.items() if i == j]
+            )
 
 
 class _828:
