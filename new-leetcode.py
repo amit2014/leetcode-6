a2ed83770e242f9dd00514808adf14cf28c762ee
yrom1 <ryanm.inbox@gmail.com>
2022-03-17 00:28:42 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -2424,9 +2424,28 @@ class _387:
     """
     # - First Unique Character in a String -
     # https://leetcode.com/problems/first-unique-character-in-a-string/
+    Given a string s, find the first non-repeating character in it and return
+    its index. If it does not exist, return -1.
+
+    Example 1:
+    Input: s = "leetcode"
+    Output: 0
+
+    Example 2:
+    Input: s = "loveleetcode"
+    Output: 2
+
+    Example 3:
+    Input: s = "aabb"
+    Output: -1
     """
 
-    ...
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+        for i, ch in enumerate(s):
+            if count[ch] == 1:
+                return i
+        return -1
 
 
 class _863:
