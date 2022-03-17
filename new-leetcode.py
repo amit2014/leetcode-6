eb3279587634a356ad568c1d26f86d9a1e3d8d0f
yrom1 <ryanm.inbox@gmail.com>
2022-03-17 19:23:21 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -2935,9 +2935,34 @@ class _605:
     """
     # - Can Place Flowers -
     # https://leetcode.com/problems/can-place-flowers/
+    You have a long flowerbed in which some of the plots are planted, and some
+    are not. However, flowers cannot be planted in adjacent plots.
+
+    Given an integer array flowerbed containing 0's and 1's, where 0 means empty and
+    1 means not empty, and an integer n, return if n new flowers can be planted in
+    the flowerbed without violating the no-adjacent-flowers rule.
+
+    Example 1:
+    Input: flowerbed = [1,0,0,0,1], n = 1
+    Output: true
+
+    Example 2:
+    Input: flowerbed = [1,0,0,0,1], n = 2
+    Output: false
+
+    NOTE There are no two adjacent flowers in flowerbed.
     """
 
-    ...
+    def canPlaceFlowers_(self, flowerbed: List[int], n: int) -> bool:
+        for i, x in enumerate(flowerbed):
+            if (
+                x == 0
+                and (i == 0 or flowerbed[i - 1] == 0)  # empty left
+                and (i + 1 == len(flowerbed) or flowerbed[i + 1] == 0)  # empty right
+            ):
+                n -= 1
+                flowerbed[i] = 1
+        return n <= 0
 
 
 class _525:
