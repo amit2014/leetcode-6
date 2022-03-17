d51ba9b1c4db316d0177fc72f68f9e6c20238c0d
yrom1 <ryanm.inbox@gmail.com>
2022-03-16 21:45:15 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -2610,9 +2610,47 @@ class _697:
     """
     # - Degree of an Array -
     # https://leetcode.com/problems/degree-of-an-array/
+    Given a non-empty array of non-negative integers nums, the degree of this array
+    is defined as the maximum frequency of any one of its elements.
+
+    Your task is to find the smallest possible length of a (contiguous) subarray of
+    nums, that has the same degree as nums.
+
+    Example 1:
+    Input: nums = [1,2,2,3,1]
+    Output: 2
+    Explanation:
+    The input array has a degree of 2 because both elements 1 and 2 appear twice.
+    Of the subarrays that have the same degree:
+    [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+    The shortest length is 2. So return 2.
+
+    Example 2:
+    Input: nums = [1,2,2,3,1,4,2]
+    Output: 6
+    Explanation:
+    The degree is 3 because the element 2 is repeated 3 times.
+    So [2,2,3,1,4,2] is the shortest subarray, therefore returning 6.
     """
 
-    ...
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        """O(n) time and space"""
+        left: Dict[int, int] = {}
+        right: Dict[int, int] = {}
+        count: Dict[int, int] = {}
+        for i, x in enumerate(nums):
+            if x not in left:
+                left[x] = i
+            right[x] = i
+            count[x] = count.get(x, 0) + 1
+
+        ans = len(nums)
+        degree = max(count.values())
+        for x in count:
+            if count[x] == degree:
+                ans = min(ans, right[x] - left[x] + 1)
+
+        return ans
 
 
 class _18:
@@ -2811,6 +2849,7 @@ class _412:
     """
 
     def fizzBuzz(self, n: int) -> List[str]:
+        """O(n) time, O(1) space"""
         ans: List[str] = []
         for i in range(1, n + 1):
             if i % 15 == 0:
