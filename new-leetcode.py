ab33ec5957ccffcb890a4720564890c4d341ca3e
yrom1 <ryanm.inbox@gmail.com>
2022-03-16 22:00:12 -0400

    Add padding to digits (`:2`) and reduce to one space between nodes

--- new-leetcode.py
+++ new-leetcode.py
@@ -1502,18 +1502,18 @@ class _938:
     [low, high].
 
     Example 1:
-          10
-      5        15
-    3    7        18
+             10
+       5           15
+    3     7           18
     Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
     Output: 32
     Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
 
     Example 2:
-                  10
-          5                15
-      3        7        13        18
-    1        6
+                         10
+             5                       15
+       3           7           13          18
+    1           6
     Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
     Output: 23
     Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
