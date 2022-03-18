5aefa80ccedc39acf333b71dc05737061f51b3be
yrom1 <ryanm.inbox@gmail.com>
2022-03-17 23:42:50 -0400

    Remove type hints from `_234.isPalindrome`

--- new-leetcode.py
+++ new-leetcode.py
@@ -3360,32 +3360,35 @@ class _234:
 
     # NOTE only use node.val and node.next of ListNode
 
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+    def isPalindrome(self, head):
         """Maintain list method: O(n) time O(1) space"""
+        if head is None:
+            return False
 
-        def middleNode(head: ListNode) -> ListNode:
+        def middleNode(head):
+            # in a->b->c->d, want b
             slow = fast = head
-            while fast.next and fast.next.next: # in a->b->c->d, want b
+            while fast.next and fast.next.next:
                 slow = slow.next
                 fast = fast.next.next
             return slow
 
-        def reverseList(head: ListNode) -> ListNode:
+        def reverseList(head):
             prev, curr = None, head
             while curr:
                 curr.next, prev, curr = prev, curr, curr.next
             return prev
 
         mid = middleNode(head)
-        tail = reverseList(mid.next) # reverse second half
+        tail = reverseList(mid.next)  # reverse second half
         _head, _tail = head, tail
         ans = True
         while ans and _tail is not None:
             if _head.val != _tail.val:
-                ans = False # don't early out so we can restore list
+                ans = False  # don't early out so we can restore list
             _head = _head.next
             _tail = _tail.next
-        mid.next = reverseList(tail) # restore list
+        mid.next = reverseList(tail)  # restore list
         return ans
 
     # def isPalindrome_(self, head: Optional[ListNode]) -> bool:
@@ -3404,6 +3407,7 @@ class _234:
     #         prev = prev.next
 
     #     return not prev
+
 
 class _289:
     """
