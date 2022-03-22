6f604e0e4404a5dfc809371101ff7045f2d98815
yrom1 <ryanm.inbox@gmail.com>
2022-03-22 12:49:04 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -3962,18 +3962,96 @@ class _706:
     """
     # - Design HashMap -
     # https://leetcode.com/problems/design-hashmap/
+    Design a HashMap without using any built-in hash table libraries.
+
+    Implement the MyHashMap class:
+    * MyHashMap() initializes the object with an empty map.
+    * void put(int key, int value) inserts a (key, value) pair into the HashMap.
+    If the key already exists in the map, update the corresponding value.
+    * int get(int key) returns the value to which the specified key is mapped,
+    or -1 if this map contains no mapping for the key.
+    * void remove(key) removes the key and its corresponding value if the map
+    contains the mapping for the key.
+
+    Example 1:
+    Input
+    ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+    [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+    Output
+    [null, null, null, 1, -1, null, 1, null, -1]
+
+    Explanation
+    MyHashMap myHashMap = new MyHashMap();
+    myHashMap.put(1, 1); // The map is now [[1,1]]
+    myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+    myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+    myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+    myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (update the existing value)
+    myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+    myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+    myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+
+    NOTE 0 <= key, value <= 106
     """
 
-    ...
+    # this question is too open ended, don't think too hard about it
+
+
+class MyHashSet:
+    w: Final = 32
+    k: Final = 10
+    M: Final = 2**k
+    W: Final = 2**w
+    a: Final = round(2 / (1 + (5 ** (1 / 2))) * W)
+
+    def __init__(self) -> None:
+        self.arr = [[] for _ in range(self.M)]
+
+    def _hash(self, key: int) -> int:
+        # fibonnaci hashing, good for the low number range given
+        return ((self.a * key) & (self.W - 1)) >> (self.w - self.k)
+
+    def add(self, key: int) -> None:
+        h = self._hash(key)
+        if key not in self.arr[h]:
+            self.arr[h].append(key)
+
+    def remove(self, key: int) -> None:
+        h = self._hash(key)
+        if key in self.arr[h]:
+            self.arr[h].remove(key)
+
+    def contains(self, key: int) -> bool:
+        h = self._hash(key)
+        return key in self.arr[h]
 
 
 class _283:
     """
     # - Move Zeroes -
     # https://leetcode.com/problems/move-zeroes/
+    Given an integer array nums, move all 0's to the end of it while maintaining the
+    relative order of the non-zero elements.
+
+    Note that you must do this in-place without making a copy of the array.
+
+    Example 1:
+    Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+
+    Example 2:
+    Input: nums = [0]
+    Output: [0]
+
+    Follow up: Could you minimize the total number of operations done?
     """
 
-    ...
+    def moveZeroes(self, nums: List[int]) -> None:
+        ii = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[i], nums[ii] = nums[ii], nums[i]
+                ii += 1
 
 
 class _1004:
