788952eb72881ae7041935e596dfea3176c02c62
yrom1 <ryanm.inbox@gmail.com>
2022-03-27 17:51:13 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -5258,9 +5258,65 @@ class _1275:
     """
     # - Find Winner on a Tic Tac Toe Game -
     # https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/
+    Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules
+    of Tic-Tac-Toe are:
+    * Players take turns placing characters into empty squares ' '.
+    * The first player A always places 'X' characters, while the second
+    player B always places 'O' characters.
+    * 'X' and 'O' characters are always placed into empty squares, never on
+    filled ones.
+    * The game ends when there are three of the same (non-empty) character
+    filling any row, column, or diagonal.
+    * The game also ends if all squares are non-empty.
+    * No more moves can be played if the game is over.
+
+    Given a 2D integer array moves where moves[i] = [rowi, coli] indicates
+    that the ith move will be played on grid[rowi][coli]. return the winner
+    of the game if it exists (A or B). In case the game ends in a draw
+    return "Draw". If there are still movements to play return "Pending".
+
+    You can assume that moves is valid (i.e., it follows the rules of
+    Tic-Tac-Toe), the grid is initially empty, and A will play first.
+
+    Example 1:
+    x
+     x
+    oox
+    Input: moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
+    Output: "A"
+    Explanation: A wins, they always play first.
+
+    Example 2:
+    xxo
+    xo
+    o
+    Input: moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
+    Output: "B"
+    Explanation: B wins.
+
+    Example 3:
+    xxo
+    oox
+    xox
+    Input: moves = [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]]
+    Output: "Draw"
+    Explanation: The game ends in a draw since there are no moves to make.
     """
 
-    ...
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        """O(m) time O(n) space, where m is len moves, n is size board"""
+        rows, cols, diag, anti = [0] * 3, [0] * 3, 0, 0
+        for turn, (x, y) in enumerate(moves):
+            player = (turn % 2) * 2 - 1
+            rows[x] += player
+            cols[y] += player
+            if x == y:
+                diag += player
+            if x + y == 2:
+                anti += player
+            if any(abs(win) == 3 for win in [rows[x], cols[y], diag, anti]):
+                return "B" if player == 1 else "A"
+        return "Draw" if len(moves) == 9 else "Pending"
 
 
 class _480:
