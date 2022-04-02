33a731e216ce04d9763724ec61ce0f7e6a1634d8
yrom1 <ryanm.inbox@gmail.com>
2022-04-02 08:46:11 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -1084,9 +1084,55 @@ class _394:
     """
     # - Decode String -
     # https://leetcode.com/problems/decode-string/
+    Given an encoded string, return its decoded string.
+
+    The encoding rule is: k[encoded_string], where the encoded_string inside the square
+    brackets is being repeated exactly k times. Note that k is guaranteed to be a
+    positive integer.
+
+    You may assume that the input string is always valid; there are no extra white
+    spaces, square brackets are well-formed, etc.
+
+    Furthermore, you may assume that the original data does not contain any digits and
+    that digits are only for those repeat numbers, k. For example, there will not be
+    input like 3a or 2[4].
+
+    Example 1:
+    Input: s = "3[a]2[bc]"
+    Output: "aaabcbc"
+
+    Example 2:
+    Input: s = "3[a2[c]]"
+    Output: "accaccacc"
+
+    Example 3:
+    Input: s = "2[abc]3[cd]ef"
+    Output: "abcabccdcdcdef"
+
+    NOTE s consists of lowercase English letters, digits, and square brackets '[]'.
+    NOTE s is guaranteed to be a valid input.
     """
 
-    ...
+    def decodeString(self, s: str) -> str:
+        stack: List[Union[str, int]] = []
+        curNum, curString = 0, ""
+        for c in s:
+            if c == "[":
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ""
+                curNum = 0
+            elif c == "]":
+                num = stack.pop()
+                assert isinstance(num, int)
+                prevString = stack.pop()
+                assert isinstance(prevString, str)
+                curString = prevString + num * curString
+            elif c.isdigit():
+                curNum = curNum * 10 + int(c)
+            else:
+                curString += c
+        return curString
 
 
 class _1396:
