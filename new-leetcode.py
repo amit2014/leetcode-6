ec4384710755807a17df23cc35cc9ed4adb17172
yrom1 <ryanm.inbox@gmail.com>
2022-04-12 09:42:12 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -85,6 +85,28 @@ def isBadVersion(version: int) -> bool:
     ...
 
 
+# NOTE we ended up type: ignore'ing all the type errors that came from this
+#      ¯\_(ツ)_/¯
+class NestedInteger:
+    def __init__(self, value: Optional[int] = None):
+        ...
+
+    def isInteger(self) -> bool:
+        ...
+
+    def add(self, elem: Union[int, NestedInteger]) -> None:
+        ...
+
+    def setInteger(self, value: int) -> None:
+        ...
+
+    def getInteger(self) -> Optional[int]:
+        ...
+
+    def getList(self) -> Optional[List[NestedInteger]]:
+        ...
+
+
 #########################################################################################
 
 
@@ -1773,9 +1795,49 @@ class _339:
     """
     # - Nested List Weight Sum -
     # https://leetcode.com/problems/nested-list-weight-sum/
+    You are given a nested list of integers nestedList. Each element is either an
+    integer or a list whose elements may also be integers or other lists.
+
+    The depth of an integer is the number of lists that it is inside of. For example,
+    the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+
+    Return the sum of each integer in nestedList multiplied by its depth.
+
+    Example 1:
+    nestedList = [[1, 1], 2, [1, 1]]
+                  ______     ______
+    depth      =   2  2       2  2
+                  _________________
+                          1
+
+    Input: nestedList = [[1,1],2,[1,1]]
+    Output: 10
+    Explanation: Four 1's at depth 2, one 2 at depth 1.
+    1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+
+    Example 2:
+    Input: nestedList = [1,[4,[6]]]
+    Output: 27
+    Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3.
+    1*1 + 4*2 + 6*3 = 27.
+
+    Example 3:
+    Input: nestedList = [0]
+    Output: 0
     """
 
-    ...
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        queue = deque(nestedList)
+        total, depth = 0, 1
+        while queue:
+            for _ in range(len(queue)):
+                nested = queue.pop()
+                if nested.isInteger():
+                    total += nested.getInteger() * depth  # type: ignore
+                else:
+                    queue.extendleft(nested.getList())  # type: ignore
+            depth += 1
+        return total
 
 
 class _1010:
