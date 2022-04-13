c9a367b7677c33166330034b206738ed3a1dab81
yrom1 <ryanm.inbox@gmail.com>
2022-04-12 20:56:40 -0400

    300

--- new-leetcode.py
+++ new-leetcode.py
@@ -33,6 +33,20 @@ class ListNode:
         self.next = next
 
 
+class Node:
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional[Node] = None,
+        right: Optional[Node] = None,
+        parent: Optional[Node] = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
 class TreeNode:
     def __init__(
         self,
@@ -1222,9 +1236,55 @@ class _236:
     """
     # - Lowest Common Ancestor of a Binary Tree -
     # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+    Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+
+    Each node will have a reference to its parent node. The definition for Node is below:
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+    According to the definition of LCA on Wikipedia: "The lowest common ancestor of two
+    nodes p and q in a tree T is the lowest node that has both p and q as descendants
+    (where we allow a node to be a descendant of itself)."
+
+    Example 1:
+                         3
+             5                       1
+       6           2           0           8
+                7     4
+    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+    Output: 3
+    Explanation: The LCA of nodes 5 and 1 is 3.
+
+    Example 2:
+    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+    Output: 5
+    Explanation: The LCA of nodes 5 and 4 is 5 since a node can be a descendant of itself according to the LCA definition.
+
+    Example 3:
+       1
+    2
+    Input: root = [1,2], p = 1, q = 2
+    Output: 1
+
+    Constraints:
+    The number of nodes in the tree is in the range [2, 105].
+    -109 <= Node.val <= 109
+    All Node.val are unique.
+    p != q
+    p and q exist in the tree.
     """
 
-    ...
+    def lowestCommonAncestor(self, p: "Node", q: "Node") -> "Node":
+        path = set()
+        while p:
+            path.add(p)
+            p = p.parent  # type: ignore
+        while q not in path:
+            q = q.parent  # type: ignore
+        return q
 
 
 class _215:
