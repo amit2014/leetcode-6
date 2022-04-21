a9c740facc89d62be2bcd5d36ac440459f2063a7
yrom1 <ryanm.inbox@gmail.com>
2022-04-21 06:41:02 -0400

    SELECT 'em all, Leetcodemon!

--- leetcode.sql
+++ leetcode.sql
@@ -7047,3 +7047,67 @@ where amount > 500
 
 2084. Drop Type 1 Orders for Customers With Type 0 Orders Medium
 
+/*
+2142. The Number of Passengers in Each Bus I Medium
+Buses and passengers arrive at the LeetCode station. If a bus arrives at the station at time `tbus` and a passenger arrived at time `tpassenger` where `tpassenger <= tbus` and the passenger did not catch any bus, the passenger will use that bus.
+
+Write an SQL query to report the number of users that used each bus.
+
+Return the result table ordered by `bus_id` in ascending order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Buses table:
++--------+--------------+
+| bus_id | arrival_time |
++--------+--------------+
+| 1      | 2            |
+| 2      | 4            |
+| 3      | 7            |
++--------+--------------+
+Passengers table:
++--------------+--------------+
+| passenger_id | arrival_time |
++--------------+--------------+
+| 11           | 1            |
+| 12           | 5            |
+| 13           | 6            |
+| 14           | 7            |
++--------------+--------------+
+Output:
++--------+----------------+
+| bus_id | passengers_cnt |
++--------+----------------+
+| 1      | 1              |
+| 2      | 0              |
+| 3      | 3              |
++--------+----------------+
+Explanation:
+- Passenger 11 arrives at time 1.
+- Bus 1 arrives at time 2 and collects passenger 11.
+
+- Bus 2 arrives at time 4 and does not collect any passengers.
+
+- Passenger 12 arrives at time 5.
+- Passenger 13 arrives at time 6.
+- Passenger 14 arrives at time 7.
+- Bus 3 arrives at time 7 and collects passengers 12, 13, and 14.
+*/
+
+WITH cte AS (
+SELECT bus_id
+	, arrival_time
+	, LAG(arrival_time, 1, 0) OVER (ORDER BY arrival_time) min_time
+FROM buses
+)
+SELECT cte.bus_id
+	, COUNT(passenger_id) passengers_cnt
+FROM cte
+	LEFT JOIN Passengers p
+		ON p.arrival_time > cte.min_time
+		AND p.arrival_time <= cte.arrival_time
+GROUP BY bus_id
+ORDER BY bus_id;
