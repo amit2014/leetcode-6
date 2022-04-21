401422a86a683b170323438c69730d3171a06702
yrom1 <ryanm.inbox@gmail.com>
2022-04-21 07:54:36 -0400

    SELECT 'em all, Leetcodemon!

--- leetcode.sql
+++ leetcode.sql
@@ -4157,8 +4157,90 @@ where count_activity <> (select max(count_activity) from cte)
     and count_activity <> (select min(count_activity) from cte)
 
 
+/*
 1364. Number of Trusted Contacts of a Customer (Medium)
 -- https://leetcode.com/problems/number-of-trusted-contacts-of-a-customer
+Write an SQL query to find the following for each invoice_id:
+    * customer_name: The name of the customer the invoice is related to.
+    * price: The price of the invoice.
+    * contacts_cnt: The number of contacts related to the customer.
+    * trusted_contacts_cnt: The number of contacts related to the customer and at the same
+    time they are customers to the shop. (i.e their email exists in the Customers table.)
+
+Return the result table ordered by invoice_id.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Customers table:
++-------------+---------------+--------------------+
+| customer_id | customer_name | email              |
++-------------+---------------+--------------------+
+| 1           | Alice         | alice@leetcode.com |
+| 2           | Bob           | bob@leetcode.com   |
+| 13          | John          | john@leetcode.com  |
+| 6           | Alex          | alex@leetcode.com  |
++-------------+---------------+--------------------+
+Contacts table:
++-------------+--------------+--------------------+
+| user_id     | contact_name | contact_email      |
++-------------+--------------+--------------------+
+| 1           | Bob          | bob@leetcode.com   |
+| 1           | John         | john@leetcode.com  |
+| 1           | Jal          | jal@leetcode.com   |
+| 2           | Omar         | omar@leetcode.com  |
+| 2           | Meir         | meir@leetcode.com  |
+| 6           | Alice        | alice@leetcode.com |
++-------------+--------------+--------------------+
+Invoices table:
++------------+-------+---------+
+| invoice_id | price | user_id |
++------------+-------+---------+
+| 77         | 100   | 1       |
+| 88         | 200   | 1       |
+| 99         | 300   | 2       |
+| 66         | 400   | 2       |
+| 55         | 500   | 13      |
+| 44         | 60    | 6       |
++------------+-------+---------+
+Output:
++------------+---------------+-------+--------------+----------------------+
+| invoice_id | customer_name | price | contacts_cnt | trusted_contacts_cnt |
++------------+---------------+-------+--------------+----------------------+
+| 44         | Alex          | 60    | 1            | 1                    |
+| 55         | John          | 500   | 0            | 0                    |
+| 66         | Bob           | 400   | 2            | 0                    |
+| 77         | Alice         | 100   | 3            | 2                    |
+| 88         | Alice         | 200   | 3            | 2                    |
+| 99         | Bob           | 300   | 2            | 0                    |
++------------+---------------+-------+--------------+----------------------+
+Explanation:
+Alice has three contacts, two of them are trusted contacts (Bob and John).
+Bob has two contacts, none of them is a trusted contact.
+Alex has one contact and it is a trusted contact (Alice).
+John doesn't have any contacts.
+*/
+WITH cte AS (
+SELECT user_id
+	, COUNT(contact_email) contacts_cnt
+	, SUM(contact_email IN (SELECT email FROM Customers)) trusted_contacts_cnt
+FROM Contacts
+GROUP BY user_id
+)
+SELECT i.invoice_id
+	, c.customer_name
+	, i.price
+	, IFNULL(contacts_cnt, 0) contacts_cnt
+	, IFNULL(trusted_contacts_cnt, 0) trusted_contacts_cnt
+FROM Invoices i
+	LEFT JOIN Customers c
+		ON i.user_id = c.customer_id
+	LEFT JOIN cte
+		ON i.user_id = cte.user_id
+ORDER BY i.invoice_id;
+
 /*
 1378. Replace Employee ID With The Unique Identifier (Easy)
 -- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier
@@ -7049,6 +7131,7 @@ where amount > 500
 
 /*
 2142. The Number of Passengers in Each Bus I Medium
+-- https://leetcode.com/problems/the-number-of-passengers-in-each-bus-i/
 Buses and passengers arrive at the LeetCode station. If a bus arrives at the station at time `tbus` and a passenger arrived at time `tpassenger` where `tpassenger <= tbus` and the passenger did not catch any bus, the passenger will use that bus.
 
 Write an SQL query to report the number of users that used each bus.
