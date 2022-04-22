8b1ef8a11930238810caf61d4807d42badf12151
yrom1 <ryanm.inbox@gmail.com>
2022-04-22 08:19:07 -0400

    300

--- leetcode.cpp
+++ leetcode.cpp
@@ -15,4 +15,24 @@ struct _1 {
     }
 };
 
+struct _146 {
+    /**
+    * Your LRUCache object will be instantiated and called as such:
+    * LRUCache* obj = new LRUCache(capacity);
+    * int param_1 = obj->get(key);
+    * obj->put(key,value);
+    */
+    LRUCache(int capacity) {
+
+    }
+
+    int get(int key) {
+
+    }
+
+    void put(int key, int value) {
+
+    }
+};
+
 int main() {}
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -xeou pipefail
-ty new-leetcode.py
+ty leetcode.py
 git add -u
 git commit -u -m "300"
 git push
