9bed9d972a31db187b1af244fcd0aa9504285958
yrom1 <ryanm.inbox@gmail.com>
2022-04-23 13:07:12 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -366,12 +366,23 @@ FROM (
 WHERE how_many_cnt_in_a_row >= 3;
 
 /*
-178. Rank Scores(Medium)
+178. Rank Scores (Medium)
 -- https://leetcode.com/problems/rank-scores/
-Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+Write an SQL query to rank the scores. The ranking should be calculated according to the following rules:
+* The scores should be ranked from the highest to the lowest.
+* If there is a tie between two scores, both should have the same ranking.
+* After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
 
+Return the result table ordered by score in descending order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Scores table:
 +----+-------+
-| Id | Score |
+| id | score |
 +----+-------+
 | 1  | 3.50  |
 | 2  | 3.65  |
@@ -380,34 +391,22 @@ Write a SQL query to rank scores. If there is a tie between two scores, both sho
 | 5  | 4.00  |
 | 6  | 3.65  |
 +----+-------+
-For example, given the above Scores table, your query should generate the following report (order by highest score):
-
-+-------+---------+
-| score | Rank    |
-+-------+---------+
-| 4.00  | 1       |
-| 4.00  | 1       |
-| 3.85  | 2       |
-| 3.65  | 3       |
-| 3.65  | 3       |
-| 3.50  | 4       |
-+-------+---------+
-Important Note: For MySQL solutions, to escape reserved words used as column names, you can use an apostrophe before and after the keyword. For example `Rank`.
+Output:
++-------+------+
+| score | rank |
++-------+------+
+| 4.00  | 1    |
+| 4.00  | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.50  | 4    |
++-------+------+
 */
 
-SELECT
-  Score,
-  DENSE_RANK() OVER (ORDER BY Score DESC) AS 'Rank'
+SELECT Score
+    , DENSE_RANK() OVER (ORDER BY Score DESC) Rank
 FROM Scores;
-
-SELECT
-  Score,
-  (SELECT
-    COUNT(DISTINCT(Score))
-  FROM Scores b
-  WHERE b.Score > a.Score) + 1 AS 'Rank'
-FROM Scores a
-ORDER by Score DESC;
 
 /*
 1412. Find the Quiet Students in All Exams (Hard)
