db871399be888bfc8ccb964d832258a7b5c1b11d
yrom1 <ryanm.inbox@gmail.com>
2022-04-23 12:41:25 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -268,10 +268,18 @@ WHERE low_fats = 'Y'
 /*
 184. Department Highest Salary (Medium)
 -- https://leetcode.com/problems/department-highest-salary/
-The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+Write an SQL query to find employees who have the highest salary in each of the departments.
 
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Employee table:
 +----+-------+--------+--------------+
-| Id | Name  | Salary | DepartmentId |
+| id | name  | salary | departmentId |
 +----+-------+--------+--------------+
 | 1  | Joe   | 70000  | 1            |
 | 2  | Jim   | 90000  | 1            |
@@ -279,41 +287,36 @@ The Employee table holds all employees. Every employee has an Id, a salary, and 
 | 4  | Sam   | 60000  | 2            |
 | 5  | Max   | 90000  | 1            |
 +----+-------+--------+--------------+
-The Department table holds all departments of the company.
-
-+----+----------+
-| Id | Name     |
-+----+----------+
-| 1  | IT       |
-| 2  | Sales    |
-+----+----------+
-Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, your SQL query should return the following rows (order of rows does not matter).
-
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output:
 +------------+----------+--------+
 | Department | Employee | Salary |
 +------------+----------+--------+
-| IT         | Max      | 90000  |
 | IT         | Jim      | 90000  |
 | Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
 +------------+----------+--------+
-Explanation:
-
-Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
 */
 
-SELECT
-  Department.Name AS Department,
-  Employee.Name AS Employee,
-  Employee.Salary
-FROM
-  Employee INNER JOIN Department ON Employee.DepartmentId = Department.Id
-WHERE
-  (DepartmentId, Salary) IN (
-  SELECT
-    DepartmentId,
-    MAX(Salary)
-  FROM Employee
-  GROUP BY DepartmentId);
+SELECT d.Name Department
+    , e.Name Employee
+    , e.Salary
+FROM Employee e
+    INNER JOIN Department d
+        ON e.DepartmentId = d.Id
+WHERE (DepartmentId, Salary) IN (
+    SELECT DepartmentId
+        , MAX(Salary)
+    FROM Employee
+    GROUP BY DepartmentId
+    );
 
 /*
 180. Consecutive Numbers (Medium)
