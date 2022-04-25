7b08ccfb37e67cb86fa0dd3f7d99ef37b8b23fb5
yrom1 <ryanm.inbox@gmail.com>
2022-04-25 19:51:54 -0400

    Run through black

--- README.py
+++ README.py
@@ -1,13 +1,15 @@
 FILES = [
-    'load',
-    'make_tree',
+    "load",
+    "make_tree",
 ]
 
+
 def get_docstring(file: str) -> str:
-    exec(f'import {file}')
+    exec(f"import {file}")
     return eval(f"{file}.__doc__")
 
-if __name__ == '__main__':
-    docs = ['# ' + file + '.py\n' + get_docstring(file) for file in FILES]
-    with open('README.md', 'w') as f:
-        f.write('\n\n---\n\n'.join(docs))
+
+if __name__ == "__main__":
+    docs = ["# " + file + ".py\n" + get_docstring(file) for file in FILES]
+    with open("README.md", "w") as f:
+        f.write("\n\n---\n\n".join(docs))
