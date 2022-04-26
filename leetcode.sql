059fe07458424db3d4611be76375004ae3ebda98
yrom1 <ryanm.inbox@gmail.com>
2022-04-26 19:22:28 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -492,12 +492,9 @@ ORDER BY 1;
 -- https://leetcode.com/problems/triangle-judgement/
 A pupil Tim gets homework to identify whether three line segments could possibly form a triangle.
 
-
 However, this assignment is very heavy because there are hundreds of records to calculate.
 
-
 Could you help Tim by writing a query to judge whether these three sides can form a triangle, assuming table triangle holds the length of the three sides x, y and z.
-
 
 | x  | y  | z  |
 |----|----|----|
@@ -510,18 +507,12 @@ For the sample data above, your query should return the follow result:
 | 10 | 20 | 15 | Yes      |
 */
 
-SELECT
-  x,
-  y,
-  z,
-  CASE
+SELECT x
+    , y
+    , z
     /* triangle inequality, assuming area_triangle > 0 */
-    WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
-    ELSE 'No'
-  END AS 'triangle'
-FROM
-  triangle
-;
+    , CASE WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes' ELSE 'No' END AS triangle
+FROM triangle;
 
 /*
 262. Trips and Users (Hard)
