5a24247caa4a3e913e91ef28024eb87fae4e2aaa
yrom1 <ryanm.inbox@gmail.com>
2022-04-27 10:18:12 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -599,7 +599,7 @@ GROUP BY Request_at;
 
 /*
 597. Friend Requests I: Overall Acceptance Rate (Easy)
-https://leetcode.com/problems/friend-requests-i-overall-acceptance-rate/
+-- https://leetcode.com/problems/friend-requests-i-overall-acceptance-rate/
 
 Write an SQL query to find the overall acceptance rate of requests, which is the number of acceptance divided by the number of requests. Return the answer rounded to 2 decimals places.
 
@@ -701,14 +701,62 @@ SELECT a.employee_id
 FROM Employees a
     INNER JOIN Employees b
     INNER JOIN Employees c
-    ON a.manager_id = b.employee_id
-    AND b.manager_id = c.employee_id
-WHERE c.manager_id = 1 /* there could be many chains  */
-    AND a.employee_id != 1 /* this is only here to exclude the head person in the company is the manager of xerself */
-;
+        ON a.manager_id = b.employee_id
+            AND b.manager_id = c.employee_id
+WHERE c.manager_id = 1
+    AND a.employee_id != 1;
 
+/*
 1179. Reformat Department Table (Easy)
 -- https://leetcode.com/problems/reformat-department-table
+Write an SQL query to reformat the table such that there is a department id column and a revenue column for each month.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Department table:
++------+---------+-------+
+| id   | revenue | month |
++------+---------+-------+
+| 1    | 8000    | Jan   |
+| 2    | 9000    | Jan   |
+| 3    | 10000   | Feb   |
+| 1    | 7000    | Feb   |
+| 1    | 6000    | Mar   |
++------+---------+-------+
+Output:
++------+-------------+-------------+-------------+-----+-------------+
+| id   | Jan_Revenue | Feb_Revenue | Mar_Revenue | ... | Dec_Revenue |
++------+-------------+-------------+-------------+-----+-------------+
+| 1    | 8000        | 7000        | 6000        | ... | null        |
+| 2    | 9000        | null        | null        | ... | null        |
+| 3    | null        | 10000       | null        | ... | null        |
++------+-------------+-------------+-------------+-----+-------------+
+Explanation: The revenue from Apr to Dec is null.
+Note that the result table has 13 columns (1 for the department id + 12 for the months).
+*/
+
+select id
+    , sum(case when month = 'jan' then revenue else null end) as Jan_Revenue
+	, sum(case when month = 'feb' then revenue else null end) as Feb_Revenue
+	, sum(case when month = 'mar' then revenue else null end) as Mar_Revenue
+	, sum(case when month = 'apr' then revenue else null end) as Apr_Revenue
+	, sum(case when month = 'may' then revenue else null end) as May_Revenue
+	, sum(case when month = 'jun' then revenue else null end) as Jun_Revenue
+	, sum(case when month = 'jul' then revenue else null end) as Jul_Revenue
+	, sum(case when month = 'aug' then revenue else null end) as Aug_Revenue
+	, sum(case when month = 'sep' then revenue else null end) as Sep_Revenue
+	, sum(case when month = 'oct' then revenue else null end) as Oct_Revenue
+	, sum(case when month = 'nov' then revenue else null end) as Nov_Revenue
+	, sum(case when month = 'dec' then revenue else null end) as Dec_Revenue
+from department
+group by id
+order by id;
+
 /*
 196. Delete Duplicate Emails (Easy)
 -- https://leetcode.com/problems/delete-duplicate-emails
