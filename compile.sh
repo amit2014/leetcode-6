81d632cd237f5676732d2b3bf6d6e115f8adf216
yrom1 <ryanm.inbox@gmail.com>
2022-05-10 14:14:51 -0400

    Switch to google style to make cpplint happy, add file argument

--- compile.sh
+++ compile.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eou pipefail
-clang-format -i --style="{IndentWidth: 4}" leetcode.cpp
-py -m cpplint --filter=-legal/copyright,-build/include_subdir,-runtime/references leetcode.cpp
-g++ -std=c++17 leetcode.cpp -o __DELETE
+
+filepath=''
+if [[ $# -eq 0 ]]; then
+    filepath='leetcode.cpp'
+else
+    filepath=$1
+fi
+
+# clang-format -i --style="{IndentWidth: 4}" filepath
+clang-format -i --style="Google" $filepath
+py -m cpplint --filter=-legal/copyright,-build/include_subdir,-runtime/references $filepath
+g++ -std=c++17 $filepath -o __DELETE
 rm __DELETE
