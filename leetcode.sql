da12ae95c1ca85b46e11f37a6ed5e616fb510996
yrom1 <ryanm.inbox@gmail.com>
2022-05-20 20:22:57 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -1198,8 +1198,57 @@ ORDER BY Company, Salary;
 +------+---------+--------+--------+---------+-------------------------------------------------------+
 */
 
+/*
 1164. Product Price at a Given Date (Medium)
 -- https://leetcode.com/problems/product-price-at-a-given-date
+Write an SQL query to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output:
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+*/
+
+SELECT product_id
+    , new_price price
+FROM Products
+WHERE (product_id, change_date) IN (
+    /* we are finding the latest change date for each product <= given date */
+    SELECT product_id
+        , MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+UNION
+SELECT product_id
+    , 10 price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
+
 1699. Number of Calls Between Two Persons (Medium)
 -- https://leetcode.com/problems/number-of-calls-between-two-persons
 1398. Customers Who Bought Products A and B but Not C (Medium)
