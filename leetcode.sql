f90fdfdcce75a93eeab852bcacd0ef94995b0224
yrom1 <ryanm.inbox@gmail.com>
2022-05-20 20:28:46 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -1249,8 +1249,51 @@ FROM Products
 GROUP BY product_id
 HAVING MIN(change_date) > '2019-08-16';
 
+/*
 1699. Number of Calls Between Two Persons (Medium)
 -- https://leetcode.com/problems/number-of-calls-between-two-persons
+Write an SQL query to report the number of calls and the total call duration between each pair of distinct persons (person1, person2) where person1 < person2.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Calls table:
++---------+-------+----------+
+| from_id | to_id | duration |
++---------+-------+----------+
+| 1       | 2     | 59       |
+| 2       | 1     | 11       |
+| 1       | 3     | 20       |
+| 3       | 4     | 100      |
+| 3       | 4     | 200      |
+| 3       | 4     | 200      |
+| 4       | 3     | 499      |
++---------+-------+----------+
+Output:
++---------+---------+------------+----------------+
+| person1 | person2 | call_count | total_duration |
++---------+---------+------------+----------------+
+| 1       | 2       | 2          | 70             |
+| 1       | 3       | 1          | 20             |
+| 3       | 4       | 4          | 999            |
++---------+---------+------------+----------------+
+Explanation:
+Users 1 and 2 had 2 calls and the total duration is 70 (59 + 11).
+Users 1 and 3 had 1 call and the total duration is 20.
+Users 3 and 4 had 4 calls and the total duration is 999 (100 + 200 + 200 + 499).
+*/
+
+SELECT IF(from_id < to_id, from_id, to_id) AS person1
+    , IF(from_id < to_id, to_id, from_id) AS person2
+    , COUNT(*) AS call_count
+    , SUM(duration) AS total_duration
+FROM Calls
+GROUP BY person1, person2;
+
 1398. Customers Who Bought Products A and B but Not C (Medium)
 -- https://leetcode.com/problems/customers-who-bought-products-a-and-b-but-not-c
 1384. Total Sales Amount by Year (Hard)
