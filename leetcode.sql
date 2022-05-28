e0c3700d306bbcf4ed518f232a4c3008790858cc
yrom1 <ryanm.inbox@gmail.com>
2022-05-28 19:23:44 -0400

    Add question before tying to fix load schema

--- leetcode.sql
+++ leetcode.sql
@@ -1363,8 +1363,77 @@ WHERE customer_id IN (
     )
 ORDER BY customer_id;
 
+/*
 1384. Total Sales Amount by Year (Hard)
 -- https://leetcode.com/problems/total-sales-amount-by-year
+Write an SQL query to report the total sales amount of each item for each year, with corresponding product_name, product_id, product_name, and report_year.
+
+Return the result table ordered by product_id and report_year.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 1          | LC Phone     |
+| 2          | LC T-Shirt   |
+| 3          | LC Keychain  |
++------------+--------------+
+Sales table:
++------------+--------------+-------------+---------------------+
+| product_id | period_start | period_end  | average_daily_sales |
++------------+--------------+-------------+---------------------+
+| 1          | 2019-01-25   | 2019-02-28  | 100                 |
+| 2          | 2018-12-01   | 2020-01-01  | 10                  |
+| 3          | 2019-12-01   | 2020-01-31  | 1                   |
++------------+--------------+-------------+---------------------+
+Output:
++------------+--------------+-------------+--------------+
+| product_id | product_name | report_year | total_amount |
++------------+--------------+-------------+--------------+
+| 1          | LC Phone     |    2019     | 3500         |
+| 2          | LC T-Shirt   |    2018     | 310          |
+| 2          | LC T-Shirt   |    2019     | 3650         |
+| 2          | LC T-Shirt   |    2020     | 10           |
+| 3          | LC Keychain  |    2019     | 31           |
+| 3          | LC Keychain  |    2020     | 31           |
++------------+--------------+-------------+--------------+
+Explanation:
+LC Phone was sold for the period of 2019-01-25 to 2019-02-28, and there are 35 days for this period. Total amount 35*100 = 3500.
+LC T-shirt was sold for the period of 2018-12-01 to 2020-01-01, and there are 31, 365, 1 days for years 2018, 2019 and 2020 respectively.
+LC Keychain was sold for the period of 2019-12-01 to 2020-01-31, and there are 31, 31 days for years 2019 and 2020 respectively.
+*/
+
+-- TODO need to review this question
+
+with recursive cte as(
+select product_id
+    , average_daily_sales
+    , period_start
+    , period_end
+from Sales
+UNION ALL
+select product_id
+    , average_daily_sales
+    , date_add(period_start, interval 1 day) period_start
+    , period_end
+from cte
+where period_start < period_end
+)
+select cte.product_id
+    , p.product_name
+    , date_format(cte.period_start, "%Y") report_year
+    , count(cte.period_start) * cte.average_daily_sales total_amount
+from cte cte
+    join Product p
+        on p.product_id = cte.product_id
+group by 1, 2, 3
+order by 1, 3;
+
 1336. Number of Transactions per Visit (Hard)
 -- https://leetcode.com/problems/number-of-transactions-per-visit
 197. Rising Temperature (Easy)
