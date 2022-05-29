891c1aa9ff603c1e229bbfdb1bc3171bea2e55bb
yrom1 <ryanm.inbox@gmail.com>
2022-05-29 03:16:44 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -1436,8 +1436,46 @@ order by 1, 3;
 
 1336. Number of Transactions per Visit (Hard)
 -- https://leetcode.com/problems/number-of-transactions-per-visit
+
+/*
 197. Rising Temperature (Easy)
 -- https://leetcode.com/problems/rising-temperature
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation:
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
+
+SELECT w1.id Id
+FROM weather w1
+    INNER JOIN weather w2
+        ON DATEDIFF(w1.recordDate, w2.recordDate) = 1 -- do not use abs here
+        AND w1.temperature > w2.temperature;
+
 1596. The Most Frequently Ordered Products for Each Customer (Medium)
 -- https://leetcode.com/problems/the-most-frequently-ordered-products-for-each-customer
 601. Human Traffic of Stadium (Hard)
