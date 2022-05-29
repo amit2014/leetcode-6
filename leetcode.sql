94d8846874722bee9b61be24686a4fa2b45ff7f4
yrom1 <ryanm.inbox@gmail.com>
2022-05-29 17:36:14 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -1476,8 +1476,98 @@ FROM weather w1
         ON DATEDIFF(w1.recordDate, w2.recordDate) = 1 -- do not use abs here
         AND w1.temperature > w2.temperature;
 
+/*
 1596. The Most Frequently Ordered Products for Each Customer (Medium)
 -- https://leetcode.com/problems/the-most-frequently-ordered-products-for-each-customer
+
+Write an SQL query to find the most frequently ordered product(s) for each customer.
+
+The result table should have the product_id and product_name for each customer_id who ordered at least one order.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Customers table:
++-------------+-------+
+| customer_id | name  |
++-------------+-------+
+| 1           | Alice |
+| 2           | Bob   |
+| 3           | Tom   |
+| 4           | Jerry |
+| 5           | John  |
++-------------+-------+
+Orders table:
++----------+------------+-------------+------------+
+| order_id | order_date | customer_id | product_id |
++----------+------------+-------------+------------+
+| 1        | 2020-07-31 | 1           | 1          |
+| 2        | 2020-07-30 | 2           | 2          |
+| 3        | 2020-08-29 | 3           | 3          |
+| 4        | 2020-07-29 | 4           | 1          |
+| 5        | 2020-06-10 | 1           | 2          |
+| 6        | 2020-08-01 | 2           | 1          |
+| 7        | 2020-08-01 | 3           | 3          |
+| 8        | 2020-08-03 | 1           | 2          |
+| 9        | 2020-08-07 | 2           | 3          |
+| 10       | 2020-07-15 | 1           | 2          |
++----------+------------+-------------+------------+
+Products table:
++------------+--------------+-------+
+| product_id | product_name | price |
++------------+--------------+-------+
+| 1          | keyboard     | 120   |
+| 2          | mouse        | 80    |
+| 3          | screen       | 600   |
+| 4          | hard disk    | 450   |
++------------+--------------+-------+
+Output:
++-------------+------------+--------------+
+| customer_id | product_id | product_name |
++-------------+------------+--------------+
+| 1           | 2          | mouse        |
+| 2           | 1          | keyboard     |
+| 2           | 2          | mouse        |
+| 2           | 3          | screen       |
+| 3           | 3          | screen       |
+| 4           | 1          | keyboard     |
++-------------+------------+--------------+
+Explanation:
+Alice (customer 1) ordered the mouse three times and the keyboard one time, so the mouse is the most frequently ordered product for them.
+Bob (customer 2) ordered the keyboard, the mouse, and the screen one time, so those are the most frequently ordered products for them.
+Tom (customer 3) only ordered the screen (two times), so that is the most frequently ordered product for them.
+Jerry (customer 4) only ordered the keyboard (one time), so that is the most frequently ordered product for them.
+John (customer 5) did not order anything, so we do not include them in the result table.
+*/
+/*
+query
+| customer_id | product_id | product_name |
+    most frequently ordered product for each customer
+    who have >= 1 order
+any order
+*/
+
+WITH cte AS (
+SELECT o.customer_id
+    , product_id
+    , any_value(p.product_name) product_name -- THIS IS SUBTLE!
+    , RANK () OVER (PARTITION BY customer_id ORDER BY COUNT(o.product_id) DESC) AS rnk
+FROM Orders o
+    INNER JOIN Products p
+        USING (product_id)
+GROUP BY customer_id, product_id
+)
+SELECT customer_id, product_id, product_name
+FROM cte
+WHERE rnk = 1;
+
+-- this is unneeded to pass:
+-- GROUP BY customer_id, product_id;
+
 601. Human Traffic of Stadium (Hard)
 183. Customers Who Never Order (Easy)
 -- https://leetcode.com/problems/customers-who-never-order
