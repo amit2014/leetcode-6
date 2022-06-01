3e0acc3c1e92de7f27c93d49c88dea9c91255266
yrom1 <ryanm.inbox@gmail.com>
2022-06-01 07:44:47 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -1800,8 +1800,61 @@ FROM cte2
 +-----------+---------+---------+------------+----------------+---------+------------+
 */
 
+/*
 1369. Get the Second Most Recent Activity (Hard)
 -- https://leetcode.com/problems/get-the-second-most-recent-activity
+Write an SQL query to show the second most recent activity of each user.
+
+If the user only has one activity, return that one. A user cannot perform more than one activity at the same time.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+UserActivity table:
++------------+--------------+-------------+-------------+
+| username   | activity     | startDate   | endDate     |
++------------+--------------+-------------+-------------+
+| Alice      | Travel       | 2020-02-12  | 2020-02-20  |
+| Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+| Alice      | Travel       | 2020-02-24  | 2020-02-28  |
+| Bob        | Travel       | 2020-02-11  | 2020-02-18  |
++------------+--------------+-------------+-------------+
+Output:
++------------+--------------+-------------+-------------+
+| username   | activity     | startDate   | endDate     |
++------------+--------------+-------------+-------------+
+| Alice      | Dancing      | 2020-02-21  | 2020-02-23  |
+| Bob        | Travel       | 2020-02-11  | 2020-02-18  |
++------------+--------------+-------------+-------------+
+Explanation:
+The most recent activity of Alice is Travel from 2020-02-24 to 2020-02-28, before that she was dancing from 2020-02-21 to 2020-02-23.
+Bob only has one record, we just take that one.
+*/
+
+/*
+query
+| username   | activity     | startDate   | endDate     |
+    second most recent activity
+    if only one activity for a user, return that
+any order
+*/
+with cte as (
+select *
+    , COUNT(activity) over(partition by username) `count`
+    , ROW_NUMBER() over(partition by username order by startdate desc) n
+from UserActivity
+)
+select username
+    , activity
+    , startDate
+    , endDate
+from cte
+where `count` < 2 or n = 2;
+
 1741. Find Total Time Spent by Each Employee (Easy)
 -- https://leetcode.com/problems/find-total-time-spent-by-each-employee
 182. Duplicate Emails (Easy)
