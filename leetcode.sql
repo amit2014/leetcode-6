4a7d9bf74307be1edf3de86cc8269e455e7ac5b1
yrom1 <ryanm.inbox@gmail.com>
2022-06-07 11:04:43 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -2409,7 +2409,50 @@ SELECT ROUND(SUM(TIV_2016), 2) AS TIV_2016
 FROM cte
 WHERE COUNT1 >= 2 AND COUNT2 = 1;
 
+/*
 596. Classes More Than 5 Students (Easy)
+-- https://leetcode.com/problems/classes-more-than-5-students/
+Write an SQL query to report all the classes that have at least five students.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output:
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation:
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+*/
+
+SELECT class
+FROM courses
+GROUP BY class
+HAVING COUNT(DISTINCT student) >= 5;
+
 1112. Highest Grade For Each Student (Medium)
 -- https://leetcode.com/problems/highest-grade-for-each-student
 1783. Grand Slam Titles (Medium)
