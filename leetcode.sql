9932ca089bec5cf6f086aa2aa6964b049adb2d50
yrom1 <ryanm.inbox@gmail.com>
2022-06-07 13:26:51 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -2501,8 +2501,63 @@ FROM cte
 WHERE max_grade = grade
 GROUP BY student_id;
 
+/*
 1783. Grand Slam Titles (Medium)
 -- https://leetcode.com/problems/grand-slam-titles
+Write an SQL query to report the number of grand slam tournaments won by each player. Do not include the players who did not win any tournament.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Players table:
++-----------+-------------+
+| player_id | player_name |
++-----------+-------------+
+| 1         | Nadal       |
+| 2         | Federer     |
+| 3         | Novak       |
++-----------+-------------+
+Championships table:
++------+-----------+---------+---------+---------+
+| year | Wimbledon | Fr_open | US_open | Au_open |
++------+-----------+---------+---------+---------+
+| 2018 | 1         | 1       | 1       | 1       |
+| 2019 | 1         | 1       | 2       | 2       |
+| 2020 | 2         | 1       | 2       | 2       |
++------+-----------+---------+---------+---------+
+Output:
++-----------+-------------+-------------------+
+| player_id | player_name | grand_slams_count |
++-----------+-------------+-------------------+
+| 2         | Federer     | 5                 |
+| 1         | Nadal       | 7                 |
++-----------+-------------+-------------------+
+Explanation:
+Player 1 (Nadal) won 7 titles: Wimbledon (2018, 2019), Fr_open (2018, 2019, 2020), US_open (2018), and Au_open (2018).
+Player 2 (Federer) won 5 titles: Wimbledon (2020), US_open (2019, 2020), and Au_open (2019, 2020).
+Player 3 (Novak) did not win anything, we did not include them in the result table.
+*/
+
+-- TODO fix only full group by
+
+SELECT ANY_VALUE(player_id) player_id
+    , ANY_VALUE(player_name) player_name
+    , SUM(player_id = Wimbledon)
+    + SUM(player_id = Fr_open)
+    + SUM(player_id = US_open)
+    + SUM(player_id = Au_open) AS grand_slams_count
+FROM Players
+    JOIN Championships
+        ON player_id = Wimbledon
+        OR player_id = Fr_open
+        OR player_id = US_open
+        OR player_id = Au_open
+GROUP BY player_id;
+
 1241. Number of Comments per Post (Easy)
 -- https://leetcode.com/problems/number-of-comments-per-post
 1285. Find the Start and End Number of Continuous Ranges (Medium)
