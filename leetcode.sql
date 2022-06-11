c86c36c9e7cd1c94483cebfa48cd2432c9dfe7e2
yrom1 <ryanm.inbox@gmail.com>
2022-06-11 12:07:22 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -2614,8 +2614,56 @@ WHERE parent.parent_id IS NULL
 GROUP BY parent.sub_id
 ORDER BY 1;
 
+/*
 1285. Find the Start and End Number of Continuous Ranges (Medium)
 -- https://leetcode.com/problems/find-the-start-and-end-number-of-continuous-ranges
+Write an SQL query to find the start and end number of continuous ranges in the table Logs.
+
+Return the result table ordered by start_id.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Logs table:
++------------+
+| log_id     |
++------------+
+| 1          |
+| 2          |
+| 3          |
+| 7          |
+| 8          |
+| 10         |
++------------+
+Output:
++------------+--------------+
+| start_id   | end_id       |
++------------+--------------+
+| 1          | 3            |
+| 7          | 8            |
+| 10         | 10           |
++------------+--------------+
+Explanation:
+The result table should contain all ranges in table Logs.
+From 1 to 3 is contained in the table.
+From 4 to 6 is missing in the table
+From 7 to 8 is contained in the table.
+Number 9 is missing from the table.
+Number 10 is contained in the table.
+*/
+
+WITH cte AS (
+SELECT log_id
+    , ROW_NUMBER() OVER (ORDER BY log_id) as row_number_log_ig
+FROM Logs
+)
+SELECT min(log_id) as start_id
+    , max(log_id) as end_id
+FROM cte
+GROUP BY log_id - row_number_log_ig
+
 595. Big Countries (Easy)
 603. Consecutive Available Seats (Easy)
 620. Not Boring Movies (Easy)
