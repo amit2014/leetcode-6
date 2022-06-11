3bbb4c83c01425530823ab548d47096376e47248
yrom1 <ryanm.inbox@gmail.com>
2022-06-11 14:29:12 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -2798,8 +2798,84 @@ WHERE id % 2 = 1
     AND description != 'boring'
 ORDER BY rating DESC;
 
+/*
 1841. League Statistics (Medium)
 -- https://leetcode.com/problems/league-statistics
+Write an SQL query to report the statistics of the league. The statistics should be built using the played matches where the winning team gets three points and the losing team gets no points. If a match ends with a draw, both teams get one point.
+
+Each row of the result table should contain:
+
+team_name - The name of the team in the Teams table.
+matches_played - The number of matches played as either a home or away team.
+points - The total points the team has so far.
+goal_for - The total number of goals scored by the team across all matches.
+goal_against - The total number of goals scored by opponent teams against this team across all matches.
+goal_diff - The result of goal_for - goal_against.
+Return the result table ordered by points in descending order. If two or more teams have the same points, order them by goal_diff in descending order. If there is still a tie, order them by team_name in lexicographical order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Teams table:
++---------+-----------+
+| team_id | team_name |
++---------+-----------+
+| 1       | Ajax      |
+| 4       | Dortmund  |
+| 6       | Arsenal   |
++---------+-----------+
+Matches table:
++--------------+--------------+-----------------+-----------------+
+| home_team_id | away_team_id | home_team_goals | away_team_goals |
++--------------+--------------+-----------------+-----------------+
+| 1            | 4            | 0               | 1               |
+| 1            | 6            | 3               | 3               |
+| 4            | 1            | 5               | 2               |
+| 6            | 1            | 0               | 0               |
++--------------+--------------+-----------------+-----------------+
+Output:
++-----------+----------------+--------+----------+--------------+-----------+
+| team_name | matches_played | points | goal_for | goal_against | goal_diff |
++-----------+----------------+--------+----------+--------------+-----------+
+| Dortmund  | 2              | 6      | 6        | 2            | 4         |
+| Arsenal   | 2              | 2      | 3        | 3            | 0         |
+| Ajax      | 4              | 2      | 5        | 9            | -4        |
++-----------+----------------+--------+----------+--------------+-----------+
+Explanation:
+Ajax (team_id=1) played 4 matches: 2 losses and 2 draws. Total points = 0 + 0 + 1 + 1 = 2.
+Dortmund (team_id=4) played 2 matches: 2 wins. Total points = 3 + 3 = 6.
+Arsenal (team_id=6) played 2 matches: 2 draws. Total points = 1 + 1 = 2.
+Dortmund is the first team in the table. Ajax and Arsenal have the same points, but since Arsenal has a higher goal_diff than Ajax, Arsenal comes before Ajax in the table.
+*/
+
+WITH cte AS (
+SELECT *
+FROM Matches M1
+UNION ALL
+SELECT away_team_id home_team_id
+    , home_team_id away_team_id
+    , away_team_goals home_team_goals
+    , home_team_goals away_team_goals
+FROM Matches M2
+)
+SELECT ANY_VALUE(T.team_name) team_name
+    , COUNT(*) matches_played
+    , SUM(CASE
+        WHEN home_team_goals > away_team_goals THEN 3
+        WHEN home_team_goals = away_team_goals THEN 1
+        WHEN home_team_goals < away_team_goals THEN 0
+    END) points
+    , SUM(home_team_goals) goal_for
+    , SUM(away_team_goals) goal_against
+    , SUM(home_team_goals) - SUM(away_team_goals) goal_diff
+FROM cte
+    INNER JOIN Teams T
+        ON cte.home_team_id = T.team_id
+GROUP BY T.team_name
+ORDER BY points DESC, goal_diff DESC, T.team_name;
+
 608. Tree Node (Medium)
 1142. User Activity for the Past 30 Days II (Easy)
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-ii
