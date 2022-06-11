40468dea9c000738b4c2d351b666caf29730353c
yrom1 <ryanm.inbox@gmail.com>
2022-06-11 13:44:21 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -2759,7 +2759,45 @@ FROM cinema a
         AND b.free = true
 ORDER BY a.seat_id;
 
+/*
 620. Not Boring Movies (Easy)
+-- https://leetcode.com/problems/not-boring-movies/
+Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring".
+
+Return the result table ordered by rating in descending order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+Output:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+Explanation:
+We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
+*/
+
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1
+    AND description != 'boring'
+ORDER BY rating DESC;
+
 1841. League Statistics (Medium)
 -- https://leetcode.com/problems/league-statistics
 608. Tree Node (Medium)
