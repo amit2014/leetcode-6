28ef9dccae197e754e9a970346423e2e1fc81c56
yrom1 <ryanm.inbox@gmail.com>
2022-06-12 20:42:35 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -2950,8 +2950,49 @@ SELECT id AS `Id`
 FROM tree
 ORDER BY `Id`;
 
+/*
 1142. User Activity for the Past 30 Days II (Easy)
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-ii
+Write an SQL query to find the average number of sessions per user for a period of 30 days ending 2019-07-27 inclusively, rounded to 2 decimal places. The sessions we want to count for a user are those with at least one activity in that time period.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 3       | 5          | 2019-07-21    | open_session  |
+| 3       | 5          | 2019-07-21    | scroll_down   |
+| 3       | 5          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output:
++---------------------------+
+| average_sessions_per_user |
++---------------------------+
+| 1.33                      |
++---------------------------+
+Explanation: User 1 and 2 each had 1 session in the past 30 days while user 3 had 2 sessions so the average is (1 + 1 + 2) / 3 = 1.33.
+*/
+
+SELECT IFNULL(ROUND(COUNT(DISTINCT session_id) / COUNT(DISTINCT user_id), 2), 0.00) AS average_sessions_per_user
+FROM Activity
+WHERE activity_date >= ('2019-07-27' - INTERVAL 29 DAY)
+    AND activity_date <= '2019-07-27';
+
 618. Students Report By Geography (Hard)
 1113. Reported Posts (Easy)
 -- https://leetcode.com/problems/reported-posts
