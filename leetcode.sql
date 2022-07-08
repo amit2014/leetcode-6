9c68815b1057b41406a31070e4b3179c377acbb6
yrom1 <ryanm.inbox@gmail.com>
2022-07-08 12:48:25 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -3948,18 +3948,7 @@ order by customer_name, customer_id, order_date desc
 
 /*
 1777. Products Price for Each Store (Easy)
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| product_id  | int     |
-| store       | enum    |
-| price       | int     |
-+-------------+---------+
-(product_id, store) is the primary key for this table.
-store is an ENUM of type ('store1', 'store2', 'store3') where each represents the store this product is available at.
-price is the price of the product at this store.
-
-
+-- https://leetcode.com/problems/products-price-for-each-store/
 Write an SQL query to find the price of each product in each store.
 
 Return the result table in any order.
@@ -3979,6 +3968,8 @@ Products table:
 | 1           | store1 | 70    |
 | 1           | store3 | 80    |
 +-------------+--------+-------+
+NOTE store is an ENUM of type ('store1', 'store2', 'store3') where each represents the store this product is available at.
+
 Output:
 +-------------+--------+--------+--------+
 | product_id  | store1 | store2 | store3 |
@@ -3986,6 +3977,7 @@ Output:
 | 0           | 95     | 100    | 105    |
 | 1           | 70     | null   | 80     |
 +-------------+--------+--------+--------+
+
 Explanation:
 Product 0 price's are 95 for store1, 100 for store2 and, 105 for store3.
 Product 1 price's are 70 for store1, 80 for store3 and, it's not sold in store2.
@@ -3996,7 +3988,6 @@ select product_id
     , max(case when store = 'store3' then price else null end) store3
 from Products
 group by product_id
-
 
 /*
 1729. Find Followers Count (Easy)
