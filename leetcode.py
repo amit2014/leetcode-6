5f396630f5216abda9c1776c3f4e875ee7ae2257
yrom1 <ryanm.inbox@gmail.com>
2022-08-11 15:00:54 -0400

    300

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,4 @@
-"""Top 300 + 2 leetcode algorithm questions by frequency."""
+"""Top 300ish leetcode algorithm questions by frequencyish."""
 
 from __future__ import annotations
 
@@ -5283,9 +5283,34 @@ class _658:
     """
     # - Find K Closest Elements -
     # https://leetcode.com/problems/find-k-closest-elements/
+    Given a sorted integer array arr, two integers k and x, return the k
+    closest integers to x in the array. The result should also be sorted
+    in ascending order.
+
+    An integer a is closer to x than an integer b if:
+        |a - x| < |b - x|, or
+        |a - x| == |b - x| and a < b
+
+    Example 1:
+    Input: arr = [1,2,3,4,5], k = 4, x = 3
+    Output: [1,2,3,4]
+
+    Example 2:
+    Input: arr = [1,2,3,4,5], k = 4, x = -1
+    Output: [1,2,3,4]
+
+    NOTE arr is sorted in ascending order.
     """
 
-    ...
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left : left + k]
 
 
 class _1047:
