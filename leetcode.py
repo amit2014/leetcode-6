072ad8c3ab88d0824e0621f6257df2813955ad21
yrom1 <ryanm.inbox@gmail.com>
2022-08-11 04:24:55 -0400

    300

--- leetcode.py
+++ leetcode.py
@@ -935,9 +935,47 @@ class _680:
     """
     # - Valid Palindrome II -
     # https://leetcode.com/problems/valid-palindrome-ii/
+    Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+    Example 1:
+
+    Input: s = "aba"
+    Output: true
+    Example 2:
+
+    Input: s = "abca"
+    Output: true
+    Explanation: You could delete the character 'c'.
+    Example 3:
+
+    Input: s = "abc"
+    Output: false
+
+    Constraints:
+
+    1 <= s.length <= 105
+    s consists of lowercase English letters.
     """
 
-    ...
+    def validPalindrome(self, s: str) -> bool:
+        """O(n) time O(1) space"""
+
+        def check_palindrome(s, i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            # Found a mismatched pair - try both deletions
+            if s[i] != s[j]:
+                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
+            i += 1
+            j -= 1
+        return True
 
 
 class _1235:
