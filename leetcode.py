cee1d23e4e88b4c12440615d0abe07b4d066ed7d
yrom1 <ryanm.inbox@gmail.com>
2022-08-11 23:38:45 -0400

    300

--- leetcode.py
+++ leetcode.py
@@ -6343,3 +6343,27 @@ class _977:
                 left += 1
             ans[i] = square**2
         return ans
+
+
+class _1304:
+    """
+    # - Find N Unique Integers Sum up to Zero -
+    # https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+    Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+    Example 1:
+    Input: n = 5
+    Output: [-7,-1,1,3,4]
+    Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+    Example 2:
+    Input: n = 3
+    Output: [-1,0,1]
+    Example 3:
+
+    Input: n = 1
+    Output: [0]
+    """
+
+    def sumZero(self, n: int) -> List[int]:
+        return [x for x in range(1 - n, n, 2)]
