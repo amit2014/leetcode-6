12a84f83fc9405238712a48450e4153caec218a8
yrom1 <ryanm.inbox@gmail.com>
2022-08-26 09:44:55 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -9375,3 +9375,57 @@ FROM cte
 		AND p.arrival_time <= cte.arrival_time
 GROUP BY bus_id
 ORDER BY bus_id;
+
+
+/*
+2329. Product Sales Analysis V
+-- https://leetcode.com/problems/product-sales-analysis-v/
+Write an SQL query that reports the spending of each user.
+
+Return the resulting table ordered by spending in descending order. In case of a tie, order them by user_id in ascending order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Sales table:
++---------+------------+---------+----------+
+| sale_id | product_id | user_id | quantity |
++---------+------------+---------+----------+
+| 1       | 1          | 101     | 10       |
+| 2       | 2          | 101     | 1        |
+| 3       | 3          | 102     | 3        |
+| 4       | 3          | 102     | 2        |
+| 5       | 2          | 103     | 3        |
++---------+------------+---------+----------+
+Product table:
++------------+-------+
+| product_id | price |
++------------+-------+
+| 1          | 10    |
+| 2          | 25    |
+| 3          | 15    |
++------------+-------+
+Output:
++---------+----------+
+| user_id | spending |
++---------+----------+
+| 101     | 125      |
+| 102     | 75       |
+| 103     | 75       |
++---------+----------+
+Explanation:
+User 101 spent 10 * 10 + 1 * 25 = 125.
+User 102 spent 3 * 15 + 2 * 15 = 75.
+User 103 spent 3 * 25 = 75.
+Users 102 and 103 spent the same amount and we break the tie by their ID while user 101 is on the top.
+*/
+
+SELECT s.user_id
+    , SUM(s.quantity * p.price) spending
+FROM Sales s
+    INNER JOIN Product p
+        USING (product_id)
+GROUP BY s.user_id
+ORDER BY 2 DESC, 1 ASC
