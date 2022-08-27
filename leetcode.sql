845d62d337bced4e5e62e3493037e8331601310e
yrom1 <ryanm.inbox@gmail.com>
2022-08-27 14:41:46 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -8824,9 +8824,64 @@ from Friendship F, T T1, T T2
 group by F.user1_id, F.user2_id
 having count(*) >=3
 
-
+/*
 1951. All the Pairs With the Maximum Number of Common Followers (Medium)
 -- https://leetcode.com/problems/all-the-pairs-with-the-maximum-number-of-common-followers
+Write an SQL query to find all the pairs of users with the maximum number of common followers. In other words, if the maximum number of common followers between any two users is maxCommon, then you have to return all pairs of users that have maxCommon common followers.
+
+The result table should contain the pairs user1_id and user2_id where user1_id < user2_id.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Relations table:
++---------+-------------+
+| user_id | follower_id |
++---------+-------------+
+| 1       | 3           |
+| 2       | 3           |
+| 7       | 3           |
+| 1       | 4           |
+| 2       | 4           |
+| 7       | 4           |
+| 1       | 5           |
+| 2       | 6           |
+| 7       | 5           |
++---------+-------------+
+Output:
++----------+----------+
+| user1_id | user2_id |
++----------+----------+
+| 1        | 7        |
++----------+----------+
+Explanation:
+Users 1 and 2 have two common followers (3 and 4).
+Users 1 and 7 have three common followers (3, 4, and 5).
+Users 2 and 7 have two common followers (3 and 4).
+Since the maximum number of common followers between any two users is 3, we return all pairs of users with three common followers, which is only the pair (1, 7). We return the pair as (1, 7), not as (7, 1).
+Note that we do not have any information about the users that follow users 3, 4, and 5, so we consider them to have 0 followers.
+*/
+
+WITH cte AS (
+SELECT r1.user_id user1_id
+    , r2.user_id user2_id
+FROM relations r1
+    INNER JOIN relations r2
+        ON r1.user_id < r2.user_id
+WHERE r1.follower_id = r2.follower_id
+)
+SELECT *
+FROM cte
+GROUP BY 1, 2
+HAVING COUNT(*) = (
+    SELECT MAX(_max)
+    FROM (SELECT COUNT(*) _max FROM cte GROUP BY user1_id, user2_id) t
+);
+
 /*
 1965. Employees With Missing Information (Easy)
 -- https://leetcode.com/problems/employees-with-missing-information
