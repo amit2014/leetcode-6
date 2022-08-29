cf774c6d9eb7f82df9cd5825eebd0cbceb29389c
yrom1 <ryanm.inbox@gmail.com>
2022-08-29 14:04:39 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -9036,8 +9036,65 @@ where salary < 30000
     and manager_id not in (select distinct employee_id from Employees)
 order by employee_id
 
-
+/*
 1988. Find Cutoff Score for Each School Medium
+-- https://leetcode.com/problems/find-cutoff-score-for-each-school/
+Every year, each school announces a minimum score requirement that a student needs to apply to it. The school chooses the minimum score requirement based on the exam results of all the students:
+    - They want to ensure that even if every student meeting the requirement applies, the school can accept everyone.
+    - They also want to maximize the possible number of students that can apply.
+    - They must use a score that is in the Exam table.
+
+Write an SQL query to report the minimum score requirement for each school. If there are multiple score values satisfying the above conditions, choose the smallest one. If the input data is not enough to determine the score, report -1.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Schools table:
++-----------+----------+
+| school_id | capacity |
++-----------+----------+
+| 11        | 151      |
+| 5         | 48       |
+| 9         | 9        |
+| 10        | 99       |
++-----------+----------+
+Exam table:
++-------+---------------+
+| score | student_count |
++-------+---------------+
+| 975   | 10            |
+| 966   | 60            |
+| 844   | 76            |
+| 749   | 76            |
+| 744   | 100           |
++-------+---------------+
+Output:
++-----------+-------+
+| school_id | score |
++-----------+-------+
+| 5         | 975   |
+| 9         | -1    |
+| 10        | 749   |
+| 11        | 744   |
++-----------+-------+
+Explanation:
+- School 5: The school's capacity is 48. Choosing 975 as the min score requirement, the school will get at most 10 applications, which is within capacity.
+- School 10: The school's capacity is 99. Choosing 844 or 749 as the min score requirement, the school will get at most 76 applications, which is within capacity. We choose the smallest of them, which is 749.
+- School 11: The school's capacity is 151. Choosing 744 as the min score requirement, the school will get at most 100 applications, which is within capacity.
+- School 9: The data given is not enough to determine the min score requirement. Choosing 975 as the min score, the school may get 10 requests while its capacity is 9. We do not have information about higher scores, hence we report -1.
+*/
+
+select school_id
+    , ifnull(min(score), -1) score
+from Schools
+    left join Exam
+        on capacity >= student_count
+group by school_id
+
 1990. Count the Number of Experiments Medium
 /*
 2004. The Number of Seniors and Juniors to Join the Company Hard
