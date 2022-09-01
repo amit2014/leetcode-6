a4d36d5c70bcb00ff6da966ae80778621529101b
yrom1 <ryanm.inbox@gmail.com>
2022-09-01 15:12:08 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -9530,6 +9530,51 @@ FROM
         ON a.r = b.r
 
 /*
+2205. The Number of Users That Are Eligible for Discount
+-- https://leetcode.com/problems/the-number-of-users-that-are-eligible-for-discount/
+A user is eligible for a discount if they had a purchase in the inclusive interval of time [startDate, endDate] with at least minAmount amount. To convert the dates to times, both dates should be considered as the start of the day (i.e., endDate = 2022-03-05 should be considered as the time 2022-03-05 00:00:00).
+
+Write an SQL query to report the number of users that are eligible for a discount.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Purchases table:
++---------+---------------------+--------+
+| user_id | time_stamp          | amount |
++---------+---------------------+--------+
+| 1       | 2022-04-20 09:03:00 | 4416   |
+| 2       | 2022-03-19 19:24:02 | 678    |
+| 3       | 2022-03-18 12:03:09 | 4523   |
+| 3       | 2022-03-30 09:43:42 | 626    |
++---------+---------------------+--------+
+startDate = 2022-03-08, endDate = 2022-03-20, minAmount = 1000
+Output:
++----------+
+| user_cnt |
++----------+
+| 1        |
++----------+
+Explanation:
+Out of the three users, only User 3 is eligible for a discount.
+ - User 1 had one purchase with at least minAmount amount, but not within the time interval.
+ - User 2 had one purchase within the time interval, but with less than minAmount amount.
+ - User 3 is the only user who had a purchase that satisfies both conditions.
+*/
+
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+  RETURN (
+    SELECT COUNT(DISTINCT user_id) user_cnt
+    FROM Purchases
+    WHERE amount >= minAmount
+        AND time_stamp BETWEEN startDate AND endDate
+  );
+END
+
+/*
 2329. Product Sales Analysis V
 -- https://leetcode.com/problems/product-sales-analysis-v/
 Write an SQL query that reports the spending of each user.
