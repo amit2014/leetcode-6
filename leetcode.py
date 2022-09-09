72fa6e2869a1a8f1aa661233909138ec3480aad0
yrom1 <ryanm.inbox@gmail.com>
2022-09-09 02:09:39 -0400

    300

--- leetcode.py
+++ leetcode.py
@@ -4025,9 +4025,51 @@ class _1091:
     """
     # - Shortest Path in Binary Matrix -
     # https://leetcode.com/problems/shortest-path-in-binary-matrix/
+    Given an n x n binary matrix grid, return the length of the shortest clear
+    path in the matrix. If there is no clear path, return -1.
+
+    A clear path in a binary matrix is a path from the top-left cell (i.e.,
+    (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+    All the visited cells of the path are 0.
+    All the adjacent cells of the path are 8-directionally connected (i.e.,
+    they are different and they share an edge or a corner).
+    The length of a clear path is the number of visited cells of this path.
+
+    Example 1:
+    Input: grid = [[0,1],[1,0]]
+    Output: 2
+
+    Example 2:
+    Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+    Output: 4
+
+    Example 3:
+    Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+    Output: -1
     """
 
-    ...
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        """O(n) time O(n) space, BFS"""
+        m = len(grid)  # NOTE n == m
+        if grid[0][0] or grid[m - 1][m - 1]:
+            return -1
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1], [-1, -1], [1, 1], [1, -1], [-1, 1]]
+        seen = set()
+        queue: Deque[tuple[int, int, int]] = deque([(0, 0, 1)])  # (x, y, dist)
+        seen.add((0, 0))
+        while queue:
+            i, j, dist = queue.popleft()
+            if i == m - 1 and j == m - 1:
+                return dist
+            for dx, dy in dirs:
+                x, y = i + dx, j + dy
+                if 0 <= x < m and 0 <= y < m and (x, y) not in seen and grid[x][y] == 0:
+                    seen.add((x, y))
+                    queue.append((x, y, dist + 1))
+        return -1
+
+    # NOTE A* solution exists
 
 
 class _37:
