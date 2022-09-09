3333116b255f825a4096aa56178feba3eda160f5
yrom1 <ryanm.inbox@gmail.com>
2022-09-09 02:47:50 -0400

    300

--- leetcode.py
+++ leetcode.py
@@ -4661,6 +4661,52 @@ class _44:
     ...
 
 
+class _916:
+    """
+    # - 916. Word Subsets -
+    # https://leetcode.com/problems/word-subsets/
+    You are given two string arrays words1 and words2.
+
+    A string b is a subset of string a if every letter in b occurs in a
+    including multiplicity.
+
+    For example, "wrr" is a subset of "warrior" but is not a subset of "world".
+    A string a from words1 is universal if for every string b in words2, b is
+    a subset of a.
+
+    Return an array of all the universal strings in words1. You may return the
+    answer in any order.
+
+    Example 1:
+    Input: words1 = ["amazon","apple","facebook","google","leetcode"],
+    words2 = ["e","o"]
+    Output: ["facebook","google","leetcode"]
+    Example 2:
+
+    Input: words1 = ["amazon","apple","facebook","google","leetcode"],
+    words2 = ["l","e"]
+    Output: ["apple","google","leetcode"]
+    NOTE words1[i] and words2[i] consist only of lowercase English letters.
+    NOTE All the strings of words1 are unique.
+    """
+
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        count = Counter()
+        for word in words2:
+            count |= Counter(word)
+        return [word for word in words1 if not count - Counter(word)]
+
+    # NOTE this solution abuses that Counters dont store negatives
+    # >>> a
+    # Counter({'a': 2, 'b': 1})
+    # >>> b
+    # Counter({'a': 1, 'b': 1})
+    # >>> a - b
+    # Counter({'a': 1})
+    # >>> b - a
+    # Counter()
+
+
 class _169:
     """
     # - Majority Element -
