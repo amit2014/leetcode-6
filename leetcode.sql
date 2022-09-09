b4a0e0f007ecf92157532a74a642e83c776b7544
yrom1 <ryanm.inbox@gmail.com>
2022-09-09 03:20:09 -0400

    300

--- leetcode.sql
+++ leetcode.sql
@@ -9281,7 +9281,56 @@ order by problem_id
 
 2041. Accepted Candidates From the Interviews Medium
 2051. The Category of Each Member in the Store Medium
+/*
 2066. Account Balance Medium
+-- https://leetcode.com/problems/account-balance/
+Write an SQL query to report the balance of each user after each transaction. You may assume that the balance of each account before any transaction is 0 and that the balance will never be below 0 at any moment.
+
+Return the result table in ascending order by account_id, then by day in case of a tie.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input:
+Transactions table:
++------------+------------+----------+--------+
+| account_id | day        | type     | amount |
++------------+------------+----------+--------+
+| 1          | 2021-11-07 | Deposit  | 2000   |
+| 1          | 2021-11-09 | Withdraw | 1000   |
+| 1          | 2021-11-11 | Deposit  | 3000   |
+| 2          | 2021-12-07 | Deposit  | 7000   |
+| 2          | 2021-12-12 | Withdraw | 7000   |
++------------+------------+----------+--------+
+Output:
++------------+------------+---------+
+| account_id | day        | balance |
++------------+------------+---------+
+| 1          | 2021-11-07 | 2000    |
+| 1          | 2021-11-09 | 1000    |
+| 1          | 2021-11-11 | 4000    |
+| 2          | 2021-12-07 | 7000    |
+| 2          | 2021-12-12 | 0       |
++------------+------------+---------+
+Explanation:
+Account 1:
+- Initial balance is 0.
+- 2021-11-07 --> deposit 2000. Balance is 0 + 2000 = 2000.
+- 2021-11-09 --> withdraw 1000. Balance is 2000 - 1000 = 1000.
+- 2021-11-11 --> deposit 3000. Balance is 1000 + 3000 = 4000.
+Account 2:
+- Initial balance is 0.
+- 2021-12-07 --> deposit 7000. Balance is 0 + 7000 = 7000.
+- 2021-12-12 --> withdraw 7000. Balance is 7000 - 7000 = 0.
+*/
+
+SELECT account_id
+    , day
+    , sum(IF(type = 'Deposit', amount, -amount)) OVER(PARTITION BY account_id ORDER BY account_id ASC, day ASC) balance
+FROM transactions;
+
+
 /*
 2072. The Winner University Easy
 There is a competition between New York University and California University. The competition is held between the same number of students from both universities. The university that has more excellent students wins the competition. If the two universities have the same number of excellent students, the competition ends in a draw.
